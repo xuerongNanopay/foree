f55dd88eec6145c36a0a11f0e54c72c782f6216e
xuerongNanopay <xuerong@nanopay.net>
2024-07-27 18:51:16 -0400

    add two implementation for NBPClient

--- backend/partner/nbp/client.go
+++ backend/partner/nbp/client.go
@@ -9,3 +9,37 @@ type NBPClient interface {
 	TransactionStatusByDate(TransactionStatusByDateRequest) (*TransactionStatusByDateResponse, error)
 	CancelTransaction(CancelTransactionRequest) (*CancelTransactionResponse, error)
 }
+
+func NewNBPClient() NBPClient {
+	return &NBPClientImpl{}
+}
+
+type NBPClientImpl struct {
+}
+
+func (*NBPClientImpl) Hello() (*HelloResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientImpl) BankList() (*BankListResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientImpl) AccountEnquiry(r AccountEnquiryRequest) (*AccountEnquiryResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientImpl) LoadRemittance(r LoadRemittanceRequest) (*LoadRemittanceResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientImpl) TransactionStatusByIds(r TransactionStatusByIdsRequest) (*TransactionStatusByIdsResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientImpl) TransactionStatusByDate(r TransactionStatusByDateRequest) (*TransactionStatusByDateResponse, error) {
+	return nil, nil
+}
+func (*NBPClientImpl) CancelTransaction(r CancelTransactionRequest) (*CancelTransactionResponse, error) {
+	return nil, nil
+}
--- backend/partner/nbp/mock.go
+++ backend/partner/nbp/mock.go
@@ -1,1 +1,35 @@
 package nbp
+
+func NewMockNBPClient() NBPClient {
+	return &NBPClientMock{}
+}
+
+type NBPClientMock struct {
+}
+
+func (*NBPClientMock) Hello() (*HelloResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientMock) BankList() (*BankListResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientMock) AccountEnquiry(r AccountEnquiryRequest) (*AccountEnquiryResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientMock) LoadRemittance(r LoadRemittanceRequest) (*LoadRemittanceResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientMock) TransactionStatusByIds(r TransactionStatusByIdsRequest) (*TransactionStatusByIdsResponse, error) {
+	return nil, nil
+}
+
+func (*NBPClientMock) TransactionStatusByDate(r TransactionStatusByDateRequest) (*TransactionStatusByDateResponse, error) {
+	return nil, nil
+}
+func (*NBPClientMock) CancelTransaction(r CancelTransactionRequest) (*CancelTransactionResponse, error) {
+	return nil, nil
+}
