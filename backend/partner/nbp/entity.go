09953a84a1be14bdb070b56df269aaa42a8c450a
xuerongNanopay <xuerong@nanopay.net>
2024-07-26 21:55:37 -0400

    add AccountEnquiryRequest

--- backend/partner/nbp/entity.go
+++ backend/partner/nbp/entity.go
@@ -37,12 +37,38 @@ type AccountEnquiry struct {
 }
 
 type HelloResponse responseWrapper[string]
+type authenticateResponse responseWrapper[authenticate]
 type BankListResponse responseWrapper[BankList]
 
-type authenticateRequest struct {
+type AuthenticateRequest struct {
 	AgencyCode int32
 	UserName   string
 	Password   string
 }
 
-// type authenticateRequest
+type BankListRequest struct {
+	token      string `json:"Token"`
+	agencyCode string `json:"Agency_Code"`
+}
+
+type AccountEnquiryRequest struct {
+	token      string `json:"Token"`
+	agencyCode string `json:"Agency_Code"`
+	AccountNo  string `json:"AccountNo"`
+	BranchCode int32  `json:"AccountNo"`
+	BankName   string `json:"BankName"`
+}
+
+type LoadRemittanceRequest struct {
+	token           string  `json:"Token"`
+	agencyCode      string  `json:"Agency_Code"`
+	GlobalId        string  `json:"Global_Id"`
+	Currency        string  `json:"Currency"`
+	Amount          float64 `json:"Amount"` //see: https://stackoverflow.com/questions/61811463/golang-encode-float-to-json-with-specified-precision
+	PmtMode         string  `json:"Pmt_Mode"`
+	RemitterName    string  `json:"Remitter_Name"`
+	RemitterAddress string  `json:"Remitter_Address"`
+	RemitterEmail   string  `json:"Remitter_Email"`
+	RemitterContact string  `json:"Remitter_Contact"`
+	RemitterIdType  string  `json:"Remitter_Id_Type"`
+}
