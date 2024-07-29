02e853740c7087e7bc4790a4542108c24d094990
xuerongNanopay <xuerong@nanopay.net>
2024-07-28 21:04:16 -0400

    implement NBPClientImpl.authenticate

--- backend/partner/nbp/client.go
+++ backend/partner/nbp/client.go
@@ -1,6 +1,9 @@
 package nbp
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -43,12 +46,14 @@ func (c *NBPClientImpl) Hello() (*HelloResponse, error) {
 	resp, err := c.httpClient.Get(url)
 
 	if err != nil {
+		//Unlikely
 		return nil, err
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
+		//Unlikely
 		return nil, err
 	}
 
@@ -62,11 +67,56 @@ func (c *NBPClientImpl) Hello() (*HelloResponse, error) {
 	return ret, nil
 }
 
-func (c *NBPClientImpl) authenticate() (*authenticate, error) {
-	url := fmt.Sprintf("%s/Authenticate?Agency_Code=%s", c.Config.AgencyCode)
+func (c *NBPClientImpl) authenticate() (*authenticateResponse, error) {
+	url := fmt.Sprintf("%s/Authenticate?Agency_Code=%s", c.Config.BaseUrl, c.Config.AgencyCode)
 	basicAuth := fmt.Sprintf("%s:%s", c.Config.Username, c.Config.Password)
+	basicAuth = base64.StdEncoding.EncodeToString([]byte(basicAuth))
+	basicAuth = fmt.Sprintf("Basic %v", basicAuth)
 
-	return nil, nil
+	r, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		//Unlikely
+		return nil, err
+	}
+
+	r.Header.Add("Authorization", basicAuth)
+
+	resp, err := c.httpClient.Do(r)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 && resp.StatusCode != 400 {
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			//Unlikely
+			return nil, err
+		}
+		raw := string(body)
+		return &authenticateResponse{
+			StatusCode:  resp.StatusCode,
+			RawResponse: raw,
+		}, nil
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		//Unlikely
+		return nil, err
+	}
+
+	auth := &authenticate{}
+	derr := json.NewDecoder(bytes.NewBuffer(body)).Decode(auth)
+	if derr != nil {
+		//Fatal: Decode json should always success, need Alert
+		return nil, derr
+	}
+
+	return &authenticateResponse{
+		StatusCode:  resp.StatusCode,
+		RawResponse: string(body),
+	}, nil
 }
 
 func (c *NBPClientImpl) updateToken() {
