17c9056e31c2bef65a2eee46df292f8b3ddbdf6f
xuerongNanopay <xuerong@nanopay.net>
2024-07-31 12:43:22 -0400

    seperate tests

--- backend/partner/nbp/entity_test.go
+++ backend/partner/nbp/entity_test.go
@@ -28,7 +28,28 @@ func TestEntityMarshal(t *testing.T) {
 	})
 
 	t.Run("NBPAmount should marshal correctly", func(t *testing.T) {
-		test1 := `{"Token":"dummy","Agency_Code":"dummy","Amount":64.64,"Remitter_Name":"xuerong","remitter_DOB":"1989-06-04"}`
+		test1 := `{"Token":"dummy","Agency_Code":"dummy","Amount":64.64,"Remitter_Name":"xuerong"}`
+
+		l := &LoadRemittanceRequest{
+			requestCommon: requestCommon{
+				Token:      "dummy",
+				AgencyCode: "dummy",
+			},
+			RemitterName: "xuerong",
+			Amount:       64.644,
+		}
+
+		s, err := json.Marshal(l)
+		if err != nil {
+			t.Errorf("LoadReimttanceRequest marshal should not get err, but got `%v`", err.Error())
+		}
+		if test1 != string(s) {
+			t.Errorf("LoadReimttanceRequest marshal incorrect, got `%v`", string(s))
+		}
+	})
+
+	t.Run("NBPDate should marshal correctly", func(t *testing.T) {
+		test1 := `{"Token":"dummy","Agency_Code":"dummy","Remitter_Name":"xuerong","remitter_DOB":"1989-06-04"}`
 		d := NBPDate(time.Date(1989, time.June, 4, 0, 0, 0, 0, time.UTC))
 
 		l := &LoadRemittanceRequest{
@@ -38,7 +59,6 @@ func TestEntityMarshal(t *testing.T) {
 			},
 			RemitterName: "xuerong",
 			RemitterDOB:  &d,
-			Amount:       64.644,
 		}
 
 		s, err := json.Marshal(l)
