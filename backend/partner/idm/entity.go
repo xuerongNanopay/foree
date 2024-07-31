952877f978018f013a62f6c8061c5e86143aba98
xuerongNanopay <xuerong@nanopay.net>
2024-07-31 16:33:45 -0400

    add idm request struct

--- backend/partner/idm/entity.go
+++ backend/partner/idm/entity.go
@@ -1,1 +1,58 @@
 package idm
+
+import "fmt"
+
+type IDMAmount float64
+
+func (a IDMAmount) MarshalJSON() ([]byte, error) {
+	s := fmt.Sprintf("%.2f", a)
+	return []byte(s), nil
+}
+
+type IDMRequest struct {
+	UserAccountIdentifier   string    `json:"man,omitempty"`
+	UserEmail               string    `json:"tea,omitempty"`
+	Ip                      string    `json:"ip,omitempty"`
+	BillingFirstName        string    `json:"bfn,omitempty"`
+	BillingMiddleName       string    `json:"bmn,omitempty"`
+	BillingLastname         string    `json:"bln,omitempty"`
+	BillingStreet           string    `json:"bsn,omitempty"`
+	BillingCity             string    `json:"bc,omitempty"`
+	BillingState            string    `json:"bs,omitempty"`
+	BillingPostcode         string    `json:"bz,omitempty"`
+	BillingCountry          string    `json:"bco,omitempty"`
+	ShippingFirstName       string    `json:"sfn,omitempty"`
+	ShippingMiddleName      string    `json:"smn,omitempty"`
+	ShippingLastname        string    `json:"sln,omitempty"`
+	ShippingStreet          string    `json:"ssn,omitempty"`
+	ShippingCity            string    `json:"sc,omitempty"`
+	ShippingState           string    `json:"ss,omitempty"`
+	ShippingPostcode        string    `json:"sz,omitempty"`
+	ShippingCountry         string    `json:"sco,omitempty"`
+	DestPhoneNumber         string    `json:"dph,omitempty"`
+	DestACHHash             string    `json:"dpach,omitempty"`
+	DestDigitalAccNOHash    string    `json:"dphash,omitempty"`
+	SrcACHHash              string    `json:"pach,omitempty"`
+	SrcDigitalAccNOHash     string    `json:"phash,omitempty"`
+	TransactionCreationTime string    `json:"tti,omitempty"`
+	Amount                  IDMAmount `json:"amt,omitempty"`
+	Currency                string    `json:"ccy,omitempty"`
+	Profile                 string    `json:"profile,omitempty"`
+	Tags                    []string  `json:"tags,omitempty"`
+	Nationality             string    `json:"nationality,omitempty"`
+	NationalId              string    `json:"nationalId,omitempty"`
+	TaxId                   string    `json:"taxId,omitempty"`
+	VoterId                 string    `json:"voterId,omitempty"`
+	DriverId                string    `json:"driverId,omitempty"`
+	PassportId              string    `json:"passportId,omitempty"`
+	BeneBankName            string    `json:"memo9,omitempty"`
+	PayoutCurrency          string    `json:"memo12,omitempty"`
+	PayoutAmount            IDMAmount `json:"memo13,omitempty"`
+	TransactionRefId        string    `json:"memo14,omitempty"`
+	IsCashPickup            bool      `json:"memo17,omitempty"`
+	RemitterPOB             string    `json:"memo18,omitempty"`
+	RemitterCOR             string    `json:"memo19,omitempty"` // sender country of residence
+	SRRelationship          string    `json:"memo20,omitempty"` // sender and receiver relationship
+	PurposeOfTransfer       string    `json:"memo15,omitempty"`
+	RemitterOccupation      string    `json:"memo16,omitempty"`
+}
