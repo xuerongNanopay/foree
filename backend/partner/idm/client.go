0fce48af959d4b5e20b6b03478665b8b7bd82e30
xuerongNanopay <xuerong@nanopay.net>
2024-07-31 18:33:40 -0400

    IDMClientImpl

--- backend/partner/idm/client.go
+++ backend/partner/idm/client.go
@@ -1,5 +1,23 @@
 package idm
 
+import "net/http"
+
 type IDMClient interface {
-	Transfer(req IDMRequest) error
+	Transfer(req IDMRequest) (*IDMResponse, error)
+}
+
+func NewIDMClient(config IDMConfig) IDMClient {
+	return &IDMClientImpl{
+		Config:     config,
+		httpClient: &http.Client{},
+	}
+}
+
+type IDMClientImpl struct {
+	Config     IDMConfig
+	httpClient *http.Client
+}
+
+func (c *IDMClientImpl) Transfer(req IDMRequest) (*IDMResponse, error) {
+	return nil, nil
 }
