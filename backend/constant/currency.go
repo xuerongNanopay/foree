ac8f5798d06192f6cf973abd08f7312f1dd5c9a0
xuerongNanopay <xuerong@nanopay.net>
2024-08-06 15:27:27 -0400

    add constant

--- backend/constant/currency.go
+++ backend/constant/currency.go
@@ -2,28 +2,32 @@ package constant
 
 var Currencies = map[string]Currency{
 	"CAD": {
-		Code:      "CAD",
-		Num:       "124",
-		Name:      "Canadian dollar",
-		Precision: 2,
+		Code:            "CAD",
+		Num:             "124",
+		Name:            "Canadian dollar",
+		PostalCodeRegex: `^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$`,
+		Precision:       2,
 	},
 	"PKR": {
-		Code:      "PKR",
-		Num:       "586",
-		Name:      "Pakistani rupee",
-		Precision: 2,
+		Code:            "PKR",
+		Num:             "586",
+		Name:            "Pakistani rupee",
+		PostalCodeRegex: `^\\d{5}$`,
+		Precision:       2,
 	},
 	"USD": {
-		Code:      "USD",
-		Num:       "840",
-		Name:      "United States doolar",
-		Precision: 2,
+		Code:            "USD",
+		Num:             "840",
+		Name:            "United States doolar",
+		PostalCodeRegex: `^\d{5}(?:[-\s]\d{4})?$`,
+		Precision:       2,
 	},
 }
 
 type Currency struct {
-	Code      string
-	Num       string
-	Name      string
-	Precision int
+	Code            string
+	Num             string
+	Name            string
+	PostalCodeRegex string
+	Precision       int
 }
