2b2194c20f371ce5b46d405d01a02532a15dcaf8
xuerongNanopay <xuerong@nanopay.net>
2024-08-06 21:33:45 -0400

    naming refactor

--- backend/app/foree/auth/referral.go
+++ backend/app/foree/auth/referral.go
@@ -9,16 +9,23 @@ const (
 	ReferralStatusDisable = "DISABLE"
 )
 
+type ReferralType string
+
+const (
+	ReferralTypeEmail ReferralType = "EMAIL"
+	ReferralTypePhone ReferralType = "PHONE"
+)
+
 type Referral struct {
-	ID         int64
-	Code       string
-	ReferType  string
-	REferValue string
-	Status     ReferralStatus
-	OwerId     string
-	RefereeId  string
-	IsRedeemed bool
-	ExpireAt   time.Time `json:"expireAt"`
-	CreateAt   time.Time `json:"createAt"`
-	UpdateAt   time.Time `json:"updateAt"`
+	ID            int64
+	Code          string
+	ReferralType  ReferralType
+	ReferralValue string
+	Status        ReferralStatus
+	OwerId        string
+	RefereeId     string
+	IsRedeemed    bool
+	ExpireAt      time.Time `json:"expireAt"`
+	CreateAt      time.Time `json:"createAt"`
+	UpdateAt      time.Time `json:"updateAt"`
 }
