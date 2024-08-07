91e4713ff55e111486829a7a6dcef5f7d86b217d
xuerongNanopay <xuerong@nanopay.net>
2024-08-07 17:54:39 -0400

    update struct

--- backend/app/foree/transaction/transaction_compliance.go
+++ backend/app/foree/transaction/transaction_compliance.go
@@ -2,35 +2,28 @@ package transaction
 
 import (
 	"time"
-
-	"xue.io/go-pay/app/foree/account"
-	"xue.io/go-pay/app/foree/types"
 )
 
 type IDMTransaction struct {
-	ID               int64
-	Status           TxStatus
-	Ip               string `json:"ip"`
-	UserAgent        string `json:"userAgent"`
-	SrcInteracAccId  int64
-	SrcInteracAcc    *account.InteracAccount
-	DescContactAccId int64
-	DescContactAcc   *account.ContactAccount
-	Amt              types.AmountData
-	ParentTxId       int64
-	OwnerId          int64
-	CreateAt         time.Time `json:"createAt"`
-	UpdateAt         time.Time `json:"updateAt"`
+	ID         int64
+	Status     TxStatus
+	Ip         string    `json:"ip"`
+	UserAgent  string    `json:"userAgent"`
+	IDMResult  string    `json:"idmResult"`
+	ParentTxId int64     `json:"parentTxId"`
+	OwnerId    int64     `json:"ownerId"`
+	CreateAt   time.Time `json:"createAt"`
+	UpdateAt   time.Time `json:"updateAt"`
 }
 
 // Large object.
 type IDMCompliance struct {
-	ID            int64
-	IDMTxId       int64
-	IDMStatusCode int
-	IDMResult     string
-	RequestJson   string
-	ResponseJson  string
-	CreateAt      time.Time `json:"createAt"`
-	UpdateAt      time.Time `json:"updateAt"`
+	ID                int64     `json:"id"`
+	IDMTxId           int64     `json:"idmTxId"`
+	IDMHttpStatusCode int       `json:"idmHttpStatusCode"`
+	IDMResult         string    `json:"idmResult"`
+	RequestJson       string    `json:"requestJson"`
+	ResponseJson      string    `json:"responseJson"`
+	CreateAt          time.Time `json:"createAt"`
+	UpdateAt          time.Time `json:"updateAt"`
 }
