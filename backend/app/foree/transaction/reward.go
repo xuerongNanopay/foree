44b1076ad13cda88fb84f218c6bc79d8a2c82d7e
xuerongNanopay <xuerong@nanopay.net>
2024-08-06 20:23:10 -0400

    naming

--- backend/app/foree/transaction/reward.go
+++ backend/app/foree/transaction/reward.go
@@ -14,15 +14,15 @@ const (
 )
 
 type Reward struct {
-	ID                 string
-	Type               string
-	Description        string
-	Amt                types.AmountData
-	Status             RewardStatus
-	IsRedeemed         bool
-	OwnerId            int64
-	ForeeTransactionId int64
-	ExpireAt           time.Time `json:"expireAt"`
-	CreateAt           time.Time `json:"createAt"`
-	UpdateAt           time.Time `json:"updateAt"`
+	ID            string
+	Type          string
+	Description   string
+	Amt           types.AmountData
+	Status        RewardStatus
+	IsRedeemed    bool
+	OwnerId       int64
+	TransactionId int64
+	ExpireAt      time.Time `json:"expireAt"`
+	CreateAt      time.Time `json:"createAt"`
+	UpdateAt      time.Time `json:"updateAt"`
 }
