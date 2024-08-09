4238740b1afbb3dadbeab15f7d24ff7276ae1cc6
xuerongNanopay <xuerong@nanopay.net>
2024-08-09 00:41:57 -0400

    add generateIdentity method

--- backend/app/foree/transaction/tx_limit_cache.go
+++ backend/app/foree/transaction/tx_limit_cache.go
@@ -1,17 +1,26 @@
 package transaction
 
 import (
+	"fmt"
 	"time"
 
 	"xue.io/go-pay/app/foree/types"
 )
 
-// TODO
 type TxLimitCache struct {
 	ID       int64
 	Identity string
-	UsdAmt   types.Amount
+	UsedAmt  types.Amount
 	MaxAmt   types.Amount
 	CreateAt time.Time `json:"createAt"`
 	UpdateAt time.Time `json:"updateAt"`
+}
+
+func generateIdentity(referenceId int64) string {
+	now := time.Now()
+	loc, err := time.LoadLocation("America/Toronto")
+	if err == nil {
+		now = now.In(loc)
+	}
+	return fmt.Sprintf("%v_%s", referenceId, now.Format(time.DateOnly))
 }
