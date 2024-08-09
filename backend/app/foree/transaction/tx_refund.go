374e175b4a8b990b7e6ce9aea9e03f56bcb1c869
xuerongNanopay <xuerong@nanopay.net>
2024-08-08 21:04:12 -0400

    scanRowIntoTxSummary

--- backend/app/foree/transaction/tx_refund.go
+++ backend/app/foree/transaction/tx_refund.go
@@ -1,22 +1,28 @@
 package transaction
 
+import (
+	"time"
+
+	"xue.io/go-pay/app/foree/account"
+)
+
 //DO this later.
 // const (
 // 	RefundType = "MANUAL_REFUND"
 // )
 
-// type ForeeRefundTx struct {
-// 	ParentTxId         int64
-// 	OwnerId            int64
-// 	RefundType         string
-// 	Status             string
-// 	IsRefunded         bool
-// 	RefundAt           time.Time
-// 	RefundInteracAccId int64
-// 	RefundInteracAcc   *account.InteracAccount
-// 	CreateAt           time.Time `json:"createAt"`
-// 	UpdateAt           time.Time `json:"updateAt"`
-// }
+type ForeeRefundTx struct {
+	ParentTxId         int64
+	OwnerId            int64
+	RefundType         string
+	Status             string
+	IsRefunded         bool
+	RefundAt           time.Time
+	RefundInteracAccId int64
+	RefundInteracAcc   *account.InteracAccount
+	CreateAt           time.Time `json:"createAt"`
+	UpdateAt           time.Time `json:"updateAt"`
+}
 
 // func NewForeeRefundTxRepo(db *sql.DB) *ForeeRefundTxRepo {
 // 	return &ForeeRefundTxRepo{db: db}
--- backend/app/foree/transaction/tx_summary.go
+++ backend/app/foree/transaction/tx_summary.go
@@ -9,9 +9,9 @@ const (
 	sQLTxSummaryGetUniqueById = `
         SELECT 
             t.id, t.summary, t.type, t.status,
-            t.src_amount, t.src_currency, t.dest_amount, t.dest_currency,
-            t.is_cancel_allowed,
-            t.parent_tx_id, t.owner_id, t.create_at, t.update_at
+            t.src_acc_summary, t.src_amount, t.src_currency, 
+            t.dest_acc_summary, t.dest_amount, t.dest_currency,
+            t.is_cancel_allowed, t.parent_tx_id, t.owner_id, t.create_at, t.update_at
         FROM tx_summary t
         where t.id = ?
     `
@@ -23,8 +23,10 @@ type TxSummary struct {
 	Type            string    `json:"type"`
 	Status          string    `json:"status"`
 	Rate            string    `json:"rate"`
+	SrcAccSummary   string    `json:"srcAccSummary"`
 	SrcAmount       string    `json:"srcAmount"`
 	SrcCurrency     string    `json:"srcCurrency"`
+	DestAccSummary  string    `json:"destAccSummary"`
 	DestAmount      string    `json:"destAmount"`
 	DestCurrency    string    `json:"desCurrency"`
 	IsCancelAllowed bool      `json:"isCancelAllowed"`
@@ -46,6 +48,20 @@ func scanRowIntoTxSummary(rows *sql.Rows) (*TxSummary, error) {
 	tx := new(TxSummary)
 	err := rows.Scan(
 		&tx.ID,
+		&tx.Summary,
+		&tx.Type,
+		&tx.Status,
+		&tx.SrcAccSummary,
+		&tx.SrcAmount,
+		&tx.SrcCurrency,
+		&tx.DestAccSummary,
+		&tx.DestAmount,
+		&tx.DestCurrency,
+		&tx.IsCancelAllowed,
+		&tx.ParentTxId,
+		&tx.OwnerId,
+		&tx.CreateAt,
+		&tx.UpdateAt,
 	)
 	if err != nil {
 		return nil, err
