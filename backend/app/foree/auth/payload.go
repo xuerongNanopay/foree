2bf68ca67c01ccc1f1b9cfa26c5bd7da924b4795
xuerongNanopay <xuerong@nanopay.net>
2024-08-10 18:51:41 -0400

    add method to validate CreateUserReq

--- backend/app/foree/auth/payload.go
+++ backend/app/foree/auth/payload.go
@@ -1,9 +1,25 @@
 package auth
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 	"time"
+
+	"github.com/go-playground/validator/v10"
+	"xue.io/go-pay/app/foree/transport"
+	"xue.io/go-pay/constant"
 )
+
+// 3600 * 24 * 365 *19
+const Second_In_Year = 31536000
+
+// letters, spaces, number and extended latin
+const NameReg = `^[a-zA-Z_0-9\u00C0-\u017F][a-zA-Z_0-9\u00C0-\u017F\s]*$`
+const NineDigitReg = `^\\d{9}$`
+
+var phoneNumberReplayer = strings.NewReplacer(" ", "", "(", "", ")", "", "-", "", "+", "")
+var validate = validator.New()
 
 // TODO: testing
 type ForeeDate struct {
@@ -53,18 +69,63 @@ type ForgetPasswordUpdateReq struct {
 }
 
 type CreateUserReq struct {
-	FirstName   string    `json:"firstName"`
+	FirstName   string    `json:"firstName" validate:"required"`
 	MiddleName  string    `json:"middleName"`
-	LastName    string    `json:"lastName"`
-	Age         int       `json:"age"`
+	LastName    string    `json:"lastName" validate:"required"`
+	Age         int       `json:"age" validate:"gte=19,lte=130"`
 	Dob         ForeeDate `json:"dob"`
-	Nationality string    `json:"nationality"`
-	Address1    string    `json:"address1"`
+	Nationality string    `json:"nationality" validate:"required"`
+	Address1    string    `json:"address1" validate:"required"`
 	Address2    string    `json:"address2"`
-	City        string    `json:"city"`
-	Province    string    `json:"province"`
-	Country     string    `json:"country"`
-	PhoneNumber string    `json:"phoneNumber"`
-	Email       string    `json:"email"`
+	City        string    `json:"city" validate:"required"`
+	Province    string    `json:"province" validate:"required"`
+	Country     string    `json:"country" validate:"required"`
+	PhoneNumber string    `json:"phoneNumber" validate:"required"`
 	AvatarUrl   string    `json:"avatarUrl"`
 }
+
+// TODO: trim name, and use allowText
+func (q *CreateUserReq) Validate() *transport.BadRequestError {
+	ret := transport.NewFormError("Invalid user creation request")
+	if err := validate.Struct(q); err != nil {
+		errors := err.(validator.ValidationErrors)
+		ret = transport.NewFormError("Invalid user creation request")
+		for _, e := range errors {
+			ret.AddDetails(e.Field(), e.Error())
+		}
+	}
+
+	age := q.Dob.Time.Unix() / int64(Second_In_Year)
+
+	if age < 19 || age > 120 {
+		ret.AddDetails("dob", "illegal age")
+	}
+
+	q.Age = int(age)
+
+	if q.Country != "CA" {
+		ret.AddDetails("country", fmt.Sprintf("invalid country `%v`", q.Country))
+	}
+
+	_, ok := constant.Regions["CA"][q.Province]
+	if !ok {
+		ret.AddDetails("province", fmt.Sprintf("invalid province `%v`", q.Province))
+	}
+
+	if len(ret.Details) > 0 {
+		return ret
+	}
+
+	phoneNumber := phoneNumberReplayer.Replace(q.PhoneNumber)
+	ok, _ = regexp.MatchString(NineDigitReg, phoneNumber)
+	if !ok {
+		ret.AddDetails("phoneNumber", fmt.Sprintf("invalid phone number `%v`", q.PhoneNumber))
+	}
+	q.PhoneNumber = phoneNumber
+
+	return nil
+}
+
+// func allowText(input string) bool {
+
+// }
