0e852823cc4639ecdd12dc162eafee825d3576e2
xuerongNanopay <xuerong@nanopay.net>
2024-08-10 02:03:37 -0400

    add referer and promotion

--- backend/app/foree/promotion/promotion.go
+++ backend/app/foree/promotion/promotion.go
@@ -1,1 +1,39 @@
 package promotion
+
+import (
+	"sync/atomic"
+	"time"
+
+	"xue.io/go-pay/app/foree/types"
+)
+
+type Promotion struct {
+	Name      string           `json:"id"`
+	Quantity  int32            `json:"limit"`
+	Amt       types.AmountData `json:"Amt"`
+	StartTime time.Time        `json:"startTime"`
+	EndTime   time.Time        `json:"endTime"`
+	IsEnable  time.Time        `json:"isEnable"`
+	CreateAt  time.Time        `json:"createAt"`
+	UpdateAt  time.Time        `json:"updateAt"`
+}
+
+func (p *Promotion) TryApply(name string) bool {
+	if name != p.Name {
+		return false
+	}
+
+	if atomic.LoadInt32(&p.Quantity) == 0 {
+		return false
+	}
+
+	atomic.AddInt32(&p.Quantity, -1)
+
+	now := time.Now().Unix()
+
+	if now > p.StartTime.Unix() || (now > p.EndTime.Unix() && !p.EndTime.IsZero()) {
+		return false
+	}
+
+	return true
+}
--- backend/app/foree/referral/referral.go
+++ backend/app/foree/referral/referral.go
@@ -1,1 +1,12 @@
 package referral
+
+import "time"
+
+type Referral struct {
+	ID           int64     `json:"id"`
+	ReferralCode string    `json:"referralCode"`
+	ReferrerId   int64     `json:"referrerId"`
+	ReferreeId   int64     `json:"referreeId"`
+	CreateAt     time.Time `json:"createAt"`
+	UpdateAt     time.Time `json:"updateAt"`
+}
