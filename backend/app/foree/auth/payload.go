029ac44601e68cee56a69c2148b642464a90d026
xuerongNanopay <xuerong@nanopay.net>
2024-08-10 17:40:49 -0400

    add validator package

--- backend/app/foree/auth/payload.go
+++ backend/app/foree/auth/payload.go
@@ -1,5 +1,31 @@
 package auth
 
+import (
+	"strings"
+	"time"
+)
+
+// TODO: testing
+type ForeeDate struct {
+	time.Time
+}
+
+func (d *ForeeDate) MarshalJSON() ([]byte, error) {
+	t := time.Time(d.Time)
+	s := t.Format(time.DateOnly)
+	return []byte("\"" + s + "\""), nil
+}
+
+func (d *ForeeDate) UnmarshalJSON(b []byte) (err error) {
+	s := strings.Trim(string(b), "\"")
+	if s == "null" {
+		d.Time = time.Time{}
+		return
+	}
+	d.Time, err = time.Parse(time.DateOnly, s)
+	return
+}
+
 type SignUpReq struct {
 	Email        string `json:"email"`
 	Password     string `json:"password"`
@@ -24,4 +50,21 @@ type LoginReq struct {
 type ForgetPasswordUpdateReq struct {
 	RetrieveCode string
 	NewPassword  string
+}
+
+type CreateUserReq struct {
+	FirstName   string    `json:"firstName"`
+	MiddleName  string    `json:"middleName"`
+	LastName    string    `json:"lastName"`
+	Age         int       `json:"age"`
+	Dob         ForeeDate `json:"dob"`
+	Nationality string    `json:"nationality"`
+	Address1    string    `json:"address1"`
+	Address2    string    `json:"address2"`
+	City        string    `json:"city"`
+	Province    string    `json:"province"`
+	Country     string    `json:"country"`
+	PhoneNumber string    `json:"phoneNumber"`
+	Email       string    `json:"email"`
+	AvatarUrl   string    `json:"avatarUrl"`
 }
