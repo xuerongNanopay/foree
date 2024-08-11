35e6f151340f2eebaac6313c032a1bc962bc97f8
xuerongNanopay <xuerong@nanopay.net>
2024-08-11 19:39:24 -0400

    remove package to avoid import cycle

--- backend/app/foree/service/account.go
+++ backend/app/foree/service/account.go
@@ -1,23 +1,24 @@
-package account
+package service
 
 import (
 	"context"
 
+	"xue.io/go-pay/app/foree/account"
 	"xue.io/go-pay/app/foree/auth"
 	"xue.io/go-pay/app/foree/transport"
 )
 
 type AccountService struct {
 	authService *auth.AuthService
-	contactRepo *ContactAccountRepo
-	interacRepo *InteracAccountRepo
+	contactRepo *account.ContactAccountRepo
+	interacRepo *account.InteracAccountRepo
 }
 
 // The method is only used by CreateUser func
 // So the permission check is already in there.
 // We don't need permission check here.
-func (a *AccountService) CreateDefaultInteracAccount(ctx context.Context, req DefaultInteracReq) transport.ForeeError {
-	acc := InteracAccount{
+func (a *AccountService) CreateDefaultInteracAccount(ctx context.Context, req account.DefaultInteracReq) transport.ForeeError {
+	acc := account.InteracAccount{
 		FirstName:   req.FirstName,
 		MiddleName:  req.MiddleName,
 		LastName:    req.LastName,
@@ -25,7 +26,7 @@ func (a *AccountService) CreateDefaultInteracAccount(ctx context.Context, req De
 		PhoneNumber: req.PhoneNumber,
 		Email:       req.Email,
 		OwnerId:     req.OwnerId,
-		Status:      AccountStatusActive,
+		Status:      account.AccountStatusActive,
 	}
 	_, err := a.interacRepo.InsertInteracAccount(acc)
 	if err != nil {
