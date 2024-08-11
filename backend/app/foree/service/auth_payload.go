8a58922cdbff3f16dfb46ad5f4fbd1ba3c3d6c3c
xuerongNanopay <xuerong@nanopay.net>
2024-08-11 19:56:56 -0400

    refactor

--- backend/app/foree/service/auth_payload.go
+++ backend/app/foree/service/auth_payload.go
@@ -243,3 +243,21 @@ func (q *CreateUserReq) Validate() *transport.BadRequestError {
 }
 
 // --------------- Response ------------------
+
+type UserDTO struct {
+	FirstName  string `json:"firstName"`
+	MiddleName string `json:"middleName"`
+	LastName   string `json:"lastName"`
+	Status     string `json:"status"`
+	AvatarUrl  string `json:"avatarUrl"`
+}
+
+func NewUserDTO(user *auth.User) *UserDTO {
+	return &UserDTO{
+		FirstName:  user.FirstName,
+		MiddleName: user.MiddleName,
+		LastName:   user.LastName,
+		Status:     string(user.Status),
+		AvatarUrl:  user.AvatarUrl,
+	}
+}
