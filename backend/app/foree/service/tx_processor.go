9a0d8e116bd9685a23c90135a0f0650b71cf2b56
xuerongNanopay <xuerong@nanopay.net>
2024-08-14 00:07:00 -0400

    add skoleton

--- backend/app/foree/service/tx_processor.go
+++ backend/app/foree/service/tx_processor.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"xue.io/go-pay/app/foree/account"
 	"xue.io/go-pay/app/foree/transaction"
 	"xue.io/go-pay/auth"
@@ -31,16 +33,39 @@ func (p *TxProcessor) LoadTx(id int64) (*transaction.ForeeTx, error) {
 }
 
 func (p *TxProcessor) ProcessTx(tx transaction.ForeeTx) (*transaction.ForeeTx, error) {
-	return p.doProcessTx(tx)
+	var err error
+	var nTx *transaction.ForeeTx
+	for {
+		nTx, err = p.doProcessTx(tx)
+		if err != nil {
+			return nil, err
+		}
+		if tx.CurStage == nTx.CurStage && nTx.CurStageStatus == tx.CurStageStatus {
+			return nTx, nil
+		}
+		// Record the history.
+		go p.recordTxHistory(transaction.NewTxHistory(nTx, ""))
+		tx = *nTx
+	}
+
+}
+
+func (p *TxProcessor) recordTxHistory(h *transaction.TxHistory) {
+	if _, err := p.txHistoryRepo.InserTxHistory(*h); err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
 
 func (p *TxProcessor) doProcessTx(tx transaction.ForeeTx) (*transaction.ForeeTx, error) {
 	if tx.Status == transaction.TxStatusInitial {
+		tx.Status = transaction.TxStatusProcessing
 		tx.CurStage = transaction.TxStageInteracCI
 		tx.CurStageStatus = transaction.TxStatusInitial
 		return &tx, nil
 	} else {
-
+		//TODO:
+		return nil, nil
 	}
 	return nil, nil
 }
