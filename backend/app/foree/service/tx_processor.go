738e2e836dce5ddd84ce8f825ef3efa836419d13
xuerongNanopay <xuerong@nanopay.net>
2024-08-13 20:59:12 -0400

    add TxProcessor

--- backend/app/foree/service/tx_processor.go
+++ backend/app/foree/service/tx_processor.go
@@ -1,3 +1,16 @@
 package service
 
+import "xue.io/go-pay/app/foree/transaction"
+
 // It is the internal service for transaction process.
+
+type TxProcesser struct {
+	interacTxRepo    *transaction.InteracCITxRepo
+	npbTxRepo        *transaction.NBPCOTxRepo
+	idmTxRepo        *transaction.IdmTxRepo
+	txHistoryRepo    *transaction.TxHistoryRepo
+	txSummaryRepo    *transaction.TxSummaryRepo
+	txLimitRepo      *transaction.TxLimitRepo
+	txLimitCacheRepo *transaction.TxLimitCacheRepo
+	foreeTxRepo      *transaction.ForeeTxRepo
+}
