f859c538fa561af6b81808626d5aef8967394a00
xuerongNanopay <xuerong@nanopay.net>
2024-08-20 12:20:13 -0400

    add CITxprocessor

--- backend/app/foree/service/tx_ci_processor.go
+++ backend/app/foree/service/tx_ci_processor.go
@@ -1,7 +1,28 @@
 package service
 
+import (
+	"context"
+
+	"xue.io/go-pay/app/foree/transaction"
+)
+
 type InteracCreditAccount struct {
 	FirstName  string
 	MiddleName string
 	LastName   string
+}
+
+type CITxProcessor struct {
+}
+
+func (p *CITxProcessor) requestPayment(ctx context.Context, tx transaction.ForeeTx) (*transaction.ForeeTx, error) {
+	return nil, nil
+}
+
+func (p *CITxProcessor) waitPaymentReceive(ctx context.Context, tx transaction.ForeeTx) (*transaction.ForeeTx, error) {
+	return nil, nil
+}
+
+func (p *CITxProcessor) doWait(ctx context.Context, tx transaction.ForeeTx) {
+	return
 }
