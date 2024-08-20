738d892fb95dd065f787391a804b84bdb6eeab75
xuerongNanopay <xuerong@nanopay.net>
2024-08-20 13:52:11 -0400

    save change

--- backend/app/foree/service/tx_ci_processor.go
+++ backend/app/foree/service/tx_ci_processor.go
@@ -3,8 +3,11 @@ package service
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
+	"xue.io/go-pay/app/foree/constant"
 	"xue.io/go-pay/app/foree/transaction"
+	"xue.io/go-pay/partner/scotia"
 )
 
 type InteracCreditAccount struct {
@@ -14,11 +17,40 @@ type InteracCreditAccount struct {
 }
 
 type CITxProcessor struct {
-	db *sql.DB
+	scotiaClient *scotia.ScotiaClient
+	foreeTxRepo  *transaction.ForeeTxRepo
+	db           *sql.DB
 }
 
 func (p *CITxProcessor) requestPayment(ctx context.Context, tx transaction.ForeeTx) (*transaction.ForeeTx, error) {
+	dTx, err := p.db.Begin()
+	if err != nil {
+		dTx.Rollback()
+		//TODO: log err
+		return nil, err
+	}
+
+	ctx = context.WithValue(ctx, constant.CKdatabaseTransaction, dTx)
+
+	// Lock transaction and safety check.
+	nForeeTx, err := p.foreeTxRepo.GetUniqueForeeTxForUpdateById(ctx, tx.ID)
+	if err != nil {
+		dTx.Rollback()
+		//TODO: log err
+		return nil, err
+	}
+
+	if nForeeTx.CurStage != transaction.TxStageInteracCI && nForeeTx.CurStageStatus != transaction.TxStatusInitial {
+		return nil, fmt.Errorf("transaction `%v` is in status `%s` at stage `%s`", nForeeTx.ID, nForeeTx.CurStageStatus, nForeeTx.Status)
+	}
+
+	// API call
+	//
 	return nil, nil
+}
+
+func (p *CITxProcessor) createRequestPaymentReq(ctx context.Context, tx transaction.ForeeTx) {
+
 }
 
 func (p *CITxProcessor) waitPaymentReceive(ctx context.Context, tx transaction.ForeeTx) (*transaction.ForeeTx, error) {
