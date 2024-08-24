1abc2a2d92e0477760f24202dbd1ec5bfcbcf349
xuerongNanopay <xuerong@nanopay.net>
2024-08-24 13:00:50 -0400

    add todo

--- backend/app/foree/service/transaction_super_service.go
+++ backend/app/foree/service/transaction_super_service.go
@@ -1,4 +1,18 @@
 package service
 
+import (
+	"database/sql"
+
+	"xue.io/go-pay/app/foree/transaction"
+)
+
 type TransactionSuperService struct {
+	db          *sql.DB
+	foreeTxRepo *transaction.ForeeTxRepo
+	idmTxRepo   *transaction.IdmTxRepo
 }
+
+//TODO: ForceCI
+//TODO: ApproveIDM
+//TODO: RejectIDM
+//TDOD: ForceNBP
