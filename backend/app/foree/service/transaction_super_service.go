9d090be58d14b01d31af1f61a928e498c4a115cf
xuerongNanopay <xuerong@nanopay.net>
2024-08-24 22:02:06 -0400

    idmStatusUpdate complete

--- backend/app/foree/service/transaction_super_service.go
+++ backend/app/foree/service/transaction_super_service.go
@@ -21,7 +21,7 @@ type TransactionSuperService struct {
 }
 
 func (t *TransactionSuperService) forceCIStatusUpdate(ctx context.Context, fTxId int64, newStatus transaction.TxStage) (*transaction.ForeeTx, error) {
-	if newStatus != transaction.TxStage(transaction.TxStatusRejected) ||
+	if newStatus != transaction.TxStage(transaction.TxStatusRejected) &&
 		newStatus != transaction.TxStage(transaction.TxStatusCompleted) {
 		return nil, fmt.Errorf("forceCIStatusUpdate -- unacceptable new transaction status %s", newStatus)
 	}
@@ -30,6 +30,7 @@ func (t *TransactionSuperService) forceCIStatusUpdate(ctx context.Context, fTxId
 		return nil, err
 	}
 
+	// Double check to avoid create DB transaction if not necessary.
 	if fTx.CurStage != transaction.TxStageInteracCI && fTx.CurStageStatus != transaction.TxStatusSent {
 		return nil, fmt.Errorf("forceCIStatusUpdate -- transaction `%v` is currently in status `%s` at stage `%s`", fTxId, fTx.CurStageStatus, fTx.CurStage)
 	}
@@ -78,7 +79,64 @@ func (t *TransactionSuperService) forceCIStatusUpdate(ctx context.Context, fTxId
 	return fTx, nil
 }
 
-//TODO: ForceCI
-//TODO: ApproveIDM
-//TODO: RejectIDM
+func (t *TransactionSuperService) idmStatusUpdate(ctx context.Context, fTxId int64, newStatus transaction.TxStage) (*transaction.ForeeTx, error) {
+	if newStatus != transaction.TxStage(transaction.TxStatusRejected) &&
+		newStatus != transaction.TxStage(transaction.TxStatusCompleted) {
+		return nil, fmt.Errorf("idmStatusUpdate -- unacceptable new transaction status %s", newStatus)
+	}
+
+	fTx, err := t.txProcessor.loadTx(fTxId, true)
+	if err != nil {
+		return nil, err
+	}
+
+	// Double check to avoid create DB transaction if not necessary.
+	if fTx.CurStage != transaction.TxStageIDM && fTx.CurStageStatus != transaction.TxStatusSuspend {
+		return nil, fmt.Errorf("forceCIStatusUpdate -- transaction `%v` is currently in status `%s` at stage `%s`", fTxId, fTx.CurStageStatus, fTx.CurStage)
+	}
+
+	dTx, err := t.db.Begin()
+	if err != nil {
+		dTx.Rollback()
+		//TODO: log err
+		return nil, err
+	}
+
+	ctx = context.WithValue(ctx, constant.CKdatabaseTransaction, dTx)
+
+	cfTx, err := t.foreeTxRepo.GetUniqueForeeTxForUpdateById(ctx, fTx.ID)
+	if err != nil {
+		dTx.Rollback()
+		//TODO: log err
+		return nil, err
+	}
+
+	// Recheck with DB transaction.
+	if cfTx.CurStage != transaction.TxStageIDM && cfTx.CurStageStatus != transaction.TxStatusSuspend {
+		dTx.Rollback()
+		return nil, fmt.Errorf("forceCIStatusUpdate -- transaction `%v` is currently in status `%s` at stage `%s`", fTxId, cfTx.CurStageStatus, cfTx.CurStage)
+	}
+
+	fTx.CI.Status = transaction.TxStatus(newStatus)
+	fTx.CurStageStatus = transaction.TxStatus(newStatus)
+
+	err = t.interacTxRepo.UpdateInteracCITxById(ctx, *fTx.CI)
+	if err != nil {
+		dTx.Rollback()
+		return nil, err
+	}
+
+	err = t.foreeTxRepo.UpdateForeeTxById(ctx, *fTx)
+	if err != nil {
+		dTx.Rollback()
+		return nil, err
+	}
+
+	if err = dTx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return fTx, nil
+}
+
 //TDOD: ForceNBP
