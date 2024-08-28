521b29e39acd8445a564447723664c21975a34e6
xuerongNanopay <xuerong@nanopay.net>
2024-08-28 16:47:03 -0400

    refactor

---
+++ backend/app/foree/constant/tx_limit.go
@@ -0,0 +1,68 @@
+package foree_constant
+
+import (
+	"xue.io/go-pay/app/foree/transaction"
+	"xue.io/go-pay/app/foree/types"
+)
+
+type TransactionLimitGroup string
+
+const (
+	TLPersonal1k TransactionLimitGroup = "TRANSACTION_PERSONAL_LIMIT_1K"
+	TLPersonal2k TransactionLimitGroup = "TRANSACTION_PERSONAL_LIMIT_2K"
+	TLPersonal3k TransactionLimitGroup = "TRANSACTION_PERSONAL_LIMIT_3K"
+	TLBO         TransactionLimitGroup = "TRANSACTION_BO_LIMIT"
+	TLAdmin      TransactionLimitGroup = "TRANSACTION_ADMIN_LIMIT"
+)
+
+// Group level transaction limit.
+var TxLimits = map[TransactionLimitGroup]transaction.TxLimit{
+	TLPersonal1k: {
+		Name: string(TLPersonal1k),
+		MinAmt: types.AmountData{
+			Amount:   types.Amount(10.0),
+			Currency: "CAD",
+		},
+		MaxAmt: types.AmountData{
+			Amount:   types.Amount(1000.0),
+			Currency: "CAD",
+		},
+		IsEnable: true,
+	},
+	TLPersonal2k: {
+		Name: string(TLPersonal2k),
+		MinAmt: types.AmountData{
+			Amount:   types.Amount(10.0),
+			Currency: "CAD",
+		},
+		MaxAmt: types.AmountData{
+			Amount:   types.Amount(2000.0),
+			Currency: "CAD",
+		},
+		IsEnable: true,
+	},
+	TLPersonal3k: {
+		Name: string(TLPersonal2k),
+		MinAmt: types.AmountData{
+			Amount:   types.Amount(10.0),
+			Currency: "CAD",
+		},
+		MaxAmt: types.AmountData{
+			Amount:   types.Amount(3000.0),
+			Currency: "CAD",
+		},
+		IsEnable: true,
+	},
+	TLBO: {
+		Name: string(TLBO),
+		MinAmt: types.AmountData{
+			Amount:   types.Amount(1.0),
+			Currency: "CAD",
+		},
+		MaxAmt: types.AmountData{
+			Amount:   types.Amount(3000.0),
+			Currency: "CAD",
+		},
+		IsEnable: true,
+	},
+}
--- backend/app/foree/transaction/tx_limit.go
+++ backend/app/foree/transaction/tx_limit.go
@@ -1,24 +1,21 @@
 package transaction
 
 import (
-	"database/sql"
 	"time"
 
 	"xue.io/go-pay/app/foree/types"
 )
 
-//TODO: Redesign? Daily limit should be account base.
-
-const (
-	sQLTxLimitGetUniqueByName = `
-		SELECT
-			l.name, l.min_amt_amount, l.min_amt_currency,
-			l.max_amt_amount, l.max_amt_currency,, l.is_enable,
-			l.created_at, l.updated_at
-		FROM tx_limit l
-		where l.name = ?
-	`
-)
+// const (
+// 	sQLTxLimitGetUniqueByName = `
+// 		SELECT
+// 			l.name, l.min_amt_amount, l.min_amt_currency,
+// 			l.max_amt_amount, l.max_amt_currency, l.is_enable,
+// 			l.created_at, l.updated_at
+// 		FROM tx_limit l
+// 		where l.name = ?
+// 	`
+// )
 
 type TxLimit struct {
 	Name      string           `json:"name"`
@@ -29,69 +26,69 @@ type TxLimit struct {
 	UpdatedAt time.Time        `json:"updatedAt"`
 }
 
-func (l *TxLimit) IsViolateLimit(amt types.AmountData) bool {
-	if !l.IsEnable {
-		return false
-	}
+// func (l *TxLimit) IsViolateLimit(amt types.AmountData) bool {
+// 	if !l.IsEnable {
+// 		return false
+// 	}
 
-	if amt.Amount < l.MinAmt.Amount {
-		return false
-	}
+// 	if amt.Amount < l.MinAmt.Amount {
+// 		return false
+// 	}
 
-	if amt.Amount > l.MaxAmt.Amount {
-		return false
-	}
+// 	if amt.Amount > l.MaxAmt.Amount {
+// 		return false
+// 	}
 
-	return true
-}
+// 	return true
+// }
 
-func NewTxLimitRepo(db *sql.DB) *TxLimitRepo {
-	return &TxLimitRepo{db: db}
-}
+// func NewTxLimitRepo(db *sql.DB) *TxLimitRepo {
+// 	return &TxLimitRepo{db: db}
+// }
 
-type TxLimitRepo struct {
-	db *sql.DB
-}
+// type TxLimitRepo struct {
+// 	db *sql.DB
+// }
 
-func (repo *TxLimitRepo) GetUniqueTxLimitByName(name string) (*TxLimit, error) {
-	rows, err := repo.db.Query(sQLTxLimitGetUniqueByName, name)
+// func (repo *TxLimitRepo) GetUniqueTxLimitByName(name string) (*TxLimit, error) {
+// 	rows, err := repo.db.Query(sQLTxLimitGetUniqueByName, name)
 
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer rows.Close()
 
-	var f *TxLimit
+// 	var f *TxLimit
 
-	for rows.Next() {
-		f, err = scanRowIntoTxLimit(rows)
-		if err != nil {
-			return nil, err
-		}
-	}
+// 	for rows.Next() {
+// 		f, err = scanRowIntoTxLimit(rows)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 	}
 
-	if f.Name == "" {
-		return nil, nil
-	}
+// 	if f.Name == "" {
+// 		return nil, nil
+// 	}
 
-	return f, nil
-}
+// 	return f, nil
+// }
 
-func scanRowIntoTxLimit(rows *sql.Rows) (*TxLimit, error) {
-	l := new(TxLimit)
-	err := rows.Scan(
-		&l.Name,
-		&l.MinAmt.Amount,
-		&l.MinAmt.Currency,
-		&l.MaxAmt.Amount,
-		&l.MaxAmt.Currency,
-		&l.IsEnable,
-		&l.CreatedAt,
-		&l.UpdatedAt,
-	)
-	if err != nil {
-		return nil, err
-	}
+// func scanRowIntoTxLimit(rows *sql.Rows) (*TxLimit, error) {
+// 	l := new(TxLimit)
+// 	err := rows.Scan(
+// 		&l.Name,
+// 		&l.MinAmt.Amount,
+// 		&l.MinAmt.Currency,
+// 		&l.MaxAmt.Amount,
+// 		&l.MaxAmt.Currency,
+// 		&l.IsEnable,
+// 		&l.CreatedAt,
+// 		&l.UpdatedAt,
+// 	)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return l, nil
-}
+// 	return l, nil
+// }
