b6976d7da0107cce6f1c693ff57fa140cad796b0
xuerongNanopay <xuerong@nanopay.net>
2024-08-29 17:05:02 -0400

    add rest wrape

--- backend/server/restful_wrapper/restful_wrapper.go
+++ backend/server/restful_wrapper/restful_wrapper.go
@@ -2,10 +2,31 @@ package restful_wrapper
 
 import (
 	"context"
+	"net/http"
 
 	"xue.io/go-pay/server/transport"
 )
 
-func RestGetWrapper[P any, Q any](f func(context.Context, P) (Q, transport.HError)) {
+func RestGetWrapper[P any, Q any](f func(context.Context, P) (Q, transport.HError)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 
+	}
+}
+
+func RestPostWrapper[P any, Q any](f func(context.Context, P) (Q, transport.HError)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func RestPutWrapper[P any, Q any](f func(context.Context, P) (Q, transport.HError)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func RestDeleteWrapper[P any, Q any](f func(context.Context, P) (Q, transport.HError)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
 }
