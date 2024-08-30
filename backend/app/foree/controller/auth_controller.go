367ea2006bab759a3452fd70af53c1ca0646470b
xuerongNanopay <xuerong@nanopay.net>
2024-08-29 22:48:22 -0400

    add help method to make code clean

--- backend/app/foree/controller/auth_controller.go
+++ backend/app/foree/controller/auth_controller.go
@@ -3,8 +3,6 @@ package foree_controller
 import (
 	"github.com/gorilla/mux"
 	"xue.io/go-pay/app/foree/service"
-	"xue.io/go-pay/auth"
-	"xue.io/go-pay/server/restful_wrapper"
 )
 
 type AuthController struct {
@@ -13,12 +11,5 @@ type AuthController struct {
 
 func (c *AuthController) RegisterRouter(router *mux.Router) {
 	// Login
-	loginHandler := restful_wrapper.RestPostWrapper(
-		c.authService.Login,
-		emptyBeforeResponse,
-		afterLogger[*auth.Session],
-		true,
-	)
-
-	router.HandleFunc("/login", loginHandler).Methods("POST")
+	router.HandleFunc("/login", simplePostWrapper(c.authService.Login)).Methods("POST")
 }
--- backend/app/foree/controller/common.go
+++ backend/app/foree/controller/common.go
@@ -1,14 +1,15 @@
 package foree_controller
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
-	"xue.io/go-pay/app/foree/service"
+	"xue.io/go-pay/server/restful_wrapper"
 	"xue.io/go-pay/server/transport"
 )
 
-func afterLogger[Q any](req service.LoginReq, resp Q, hErr transport.HError) {
+func afterLogger[P any, Q any](req P, resp Q, hErr transport.HError) {
 	if v, is := hErr.(*transport.InteralServerError); is {
 		// use logger.
 		fmt.Print(v.OriginalError.Error())
@@ -19,4 +20,22 @@ func afterLogger[Q any](req service.LoginReq, resp Q, hErr transport.HError) {
 
 func emptyBeforeResponse[Q any](w http.ResponseWriter, resp Q) http.ResponseWriter {
 	return w
+}
+
+func simpleGetWrapper[P any, Q any](handler func(context.Context, P) (Q, transport.HError)) func(http.ResponseWriter, *http.Request) {
+	return restful_wrapper.RestGetWrapper(
+		handler,
+		emptyBeforeResponse,
+		afterLogger[P, Q],
+		true,
+	)
+}
+
+func simplePostWrapper[P any, Q any](handler func(context.Context, P) (Q, transport.HError)) func(http.ResponseWriter, *http.Request) {
+	return restful_wrapper.RestPostWrapper(
+		handler,
+		emptyBeforeResponse,
+		afterLogger[P, Q],
+		true,
+	)
 }
