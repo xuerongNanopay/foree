e327fb0f8509fb1636211c86a746bcbdde8ab177
xuerongNanopay <xuerong@nanopay.net>
2024-09-01 12:44:02 -0400

    add scotiaCient mock

--- backend/partner/scotia/mock.go
+++ backend/partner/scotia/mock.go
@@ -1,1 +1,28 @@
 package scotia
+
+func NewMockScotiaClient() ScotiaClient {
+	return &ScotiaClientMock{}
+}
+
+type ScotiaClientMock struct {
+	config ScotiaConfig
+}
+
+func (s *ScotiaClientMock) RequestPayment(req RequestPaymentRequest) (*RequestPaymentResponse, error) {
+	return nil, nil
+}
+func (s *ScotiaClientMock) PaymentDetail(req PaymentDetailRequest) (*PaymentDetailResponse, error) {
+	return nil, nil
+}
+func (s *ScotiaClientMock) PaymentStatus(req PaymentStatusRequest) (*PaymentStatusResponse, error) {
+	return nil, nil
+}
+func (s *ScotiaClientMock) CancelPayment(req CancelPaymentRequest) (*CancelPaymentResponse, error) {
+	return nil, nil
+}
+func (s *ScotiaClientMock) GetConfigs() map[string]string {
+	s.config.ShowConfigs()
+}
+func (s *ScotiaClientMock) SetConfig(key string, value string) {
+	s.config.SetConfig(key, value)
+}
