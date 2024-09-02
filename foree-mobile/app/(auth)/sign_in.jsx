d89de8e19bb7c13b3b1440b5a2048c03bdd58aa6
xuerongNanopay <xuerong@nanopay.net>
2024-09-02 19:10:33 -0400

    add sign in

--- foree-mobile/app/(auth)/sign_in.jsx
+++ foree-mobile/app/(auth)/sign_in.jsx
@@ -1,16 +1,46 @@
 import { ScrollView, StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
+import FormField from '../../components/FormField'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
   return (
-    <SafeAreaView className="h-full">
+    <SafeAreaView className="h-full bg-white">
       <ScrollView>
-        <View class="w-full justify-center min-h-[85vh] px-2 my-6">
+        <View className="w-full justify-center h-full px-2 my-2">
           <Image
             source={images.logo}
+            resizeMode='contain'
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-slate-700 text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({
+              ...form,
+              email:e
+            })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({
+              ...form,
+              password:e
+            })}
+            otherStyles="mt-7"
           />
         </View>
       </ScrollView>
---
+++ foree-mobile/components/FormField.jsx
@@ -0,0 +1,43 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
+
+import { icons } from '../constants'
+
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
+  const [showPassword, setShowPassword] = useState(false)
+  return (
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="test-base test-gray-100 font-pmedium">{title}</Text>
+      <View 
+        className="
+          w-full h-12 px-4 bg-slate-100 border-2 
+          border-slate-400 rounded-2xl focus:border-secondary-200 
+          items-center flex-row
+        "
+      >
+        <TextInput
+          className="flex-1 font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#BDBDBD"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === 'Password' && !showPassword}
+        />
+
+        {title === 'Password' && (
+          <TouchableOpacity
+            onPress={()=> setShowPassword(!showPassword)}
+          >
+            <Image 
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-7 h-7"
+              resizeMode='contain'
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  )
+}
+
+export default FormField
