63d2ed63f68b9d15d930fe1b821e0481f3e98af9
xuerongNanopay <xuerong@nanopay.net>
2024-09-02 13:18:19 -0400

    initial tailwind

--- foree-mobile/app/index.jsx
+++ foree-mobile/app/index.jsx
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Foree!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Foree!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Go to probile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
