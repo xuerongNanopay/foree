8c189f204cff4cc94af48aa3c8f0e63b0a6398a4
xuerongNanopay <xuerong@nanopay.net>
2024-09-03 11:27:58 -0400

    add verifycode route

---
+++ foree-mobile/app/(auth)/verify_code.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const VerifyCode = () => {
+  return (
+    <View>
+      <Text>VerifyCode</Text>
+    </View>
+  )
+}
+
+export default VerifyCode
