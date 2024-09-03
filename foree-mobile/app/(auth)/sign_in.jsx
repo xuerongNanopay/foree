c823a247abc9c8fd36f42aaa22a81e8bc6692e8e
xuerongNanopay <xuerong@nanopay.net>
2024-09-02 22:01:49 -0400

    update sign in

--- foree-mobile/app/(auth)/sign_in.jsx
+++ foree-mobile/app/(auth)/sign_in.jsx
@@ -28,45 +28,62 @@ const SignIn = () => {
     // <ScrollView>
     <SafeAreaView className="h-full bg-[#004d40]">
       <ScrollView className="bg-slate-100">
-        <View className="w-full justify-center min-h-[85vh] px-2 my-2">
-          <Image
-            source={images.logo}
-            resizeMode='contain'
-            className="w-[115px] h-[35px]"
-          />
-          <Text className="text-2xl text-slate-700 text-semibold mt-10 font-psemibold">
-            Log in to Aora
-          </Text>
-          <FormField
-            title="Email"
-            value={form.email}
-            handleChangeText={(e) => setForm({
-              ...form,
-              email:e
-            })}
-            otherStyles="mt-7"
-            keyboardType="email-address"
-          />
-          <FormField
-            title="Password"
-            value={form.password}
-            handleChangeText={(e) => setForm({
-              ...form,
-              password:e
-            })}
-            otherStyles="mt-7"
-          />
-          <CustomButton
-            title="Sign In"
-            handlePress={submit}
-            containerStyles="mt-7"
-            isLoading={isSubmitting}
-          />
-          <View className="justify-center items-center pt-4 flex-row gap-2">
-            <Text className="text-lg text-gray-100 font-pregular">
-              Don't have account?
-            </Text>
-            <Link href="/sign_up" className='"text-lg font-psemibold text-secondary'>Sign Up</Link>
+        <View className="w-full">
+          <View className="px-4 mt-5">
+            <Image
+              source={images.logoSmall}
+              resizeMode='contain'
+              className="w-[36px] h-[36px]"
+            />
+            <View>
+              <Text className="mt-6 text-secondary text-left font-psemibold text-xl">
+                Sign-up & receive $44 to try the fastest global transfer
+              </Text>
+              <Text className="mt-4 text-secondary text-left font-bold">
+                &#10003; $0 fees and best FX rates
+              </Text>
+              <Text className="mt-4 text-secondary text-left font-bold">
+                &#10003; Transfer to 35+ USA banks
+              </Text>
+              <Text className="mt-4 text-secondary text-left font-bold">
+                &#10003; Cash pick-ups from 1500+ USA branches
+              </Text>
+            </View>
+          </View>
+          <View>
+            <View>
+              <FormField
+                title="Email"
+                value={form.email}
+                handleChangeText={(e) => setForm({
+                  ...form,
+                  email:e
+                })}
+                otherStyles="mt-7"
+                keyboardType="email-address"
+              />
+              <FormField
+                title="Password"
+                value={form.password}
+                handleChangeText={(e) => setForm({
+                  ...form,
+                  password:e
+                })}
+                otherStyles="mt-7"
+              />
+              <CustomButton
+                title="Sign In"
+                handlePress={submit}
+                containerStyles="mt-7"
+                isLoading={isSubmitting}
+              />
+              <View className="justify-center items-center pt-4 flex-row gap-2">
+                <Text className="text-lg text-gray-100 font-pregular">
+                  Don't have account?
+                </Text>
+                <Link href="/sign_up" className='"text-lg font-psemibold text-secondary'>Sign Up</Link>
+              </View>
+            </View>
           </View>
         </View>
       </ScrollView>
