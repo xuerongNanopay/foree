06e85ede654b3592a8aa867c18515989f29cc273
xuerongNanopay <xuerong@nanopay.net>
2024-09-02 23:40:31 -0400

    complete forget_password

--- foree-mobile/app/(auth)/forget_password.jsx
+++ foree-mobile/app/(auth)/forget_password.jsx
@@ -1,10 +1,25 @@
 import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
+import CustomButton from '../../components/CustomButton'
+import FormField from '../../components/FormField'
 
 const ForgetPassword = () => {
+  const [form, setForm] = useState({
+    email: ''
+  })
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+    setIsSubmitting(true)
+    setTimeout(() => {
+      setIsSubmitting(false)
+    }, 2000);
+  }
+
   return (
     <SafeAreaView className="h-full">
       <ScrollView
@@ -23,6 +38,25 @@ const ForgetPassword = () => {
           <Text className="font-pregular text-center m-4">
             Enter the mail you used to create your account in order to reset your password.
           </Text>
+
+          <View className="px-2">
+            <FormField
+              title="Email"
+              value={form.email}
+              handleChangeText={(e) => setForm({
+                ...form,
+                email:e
+              })}
+              otherStyles="mt-1"
+              keyboardType="email-address"
+            />
+            <CustomButton
+              title="Submit"
+              handlePress={submit}
+              containerStyles="mt-7"
+              isLoading={isSubmitting}
+            />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
