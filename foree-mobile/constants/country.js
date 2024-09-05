39ceccf8666f036f2c1f2d5b8d3f4d1702eeef41
xuerongNanopay <xuerong@nanopay.net>
2024-09-05 16:21:11 -0400

    add country constant

---
+++ foree-mobile/constants/country.js
@@ -0,0 +1,28 @@
+const Countries = {
+	"CA": {
+		isoCode:  "CA",
+		isoCode3: "CAN",
+		Num:   "124",
+		Name:  "Canada",
+    phoneRegex: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/,
+    postalCodeRegex: /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/
+	},
+	"PK": {
+		isoCode:  "PK",
+		isoCode3: "PAK",
+		Num:   "586",
+		Name:  "Pakistan",
+    phoneRegex: /^((\+92)?(0092)?(92)?(0)?)(3)([0-9]{9})$/gm,
+    postalCodeRegex: /^\\d{5}$/
+	},
+	"US": {
+		isoCode:  "US",
+		isoCode3: "USA",
+		Num:   "840",
+		Name:  "United States of America",
+    phoneRegex: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/,
+    postalCodeRegex: /^\d{5}(?:[-\s]\d{4})?$/
+	},
+}
+
+export default Countries
