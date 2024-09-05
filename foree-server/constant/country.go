c624b51940c0c2a82448c61187443879ba728957
xuerongNanopay <xuerong@nanopay.net>
2024-09-05 16:26:44 -0400

    rename constant

--- foree-server/constant/country.go
+++ foree-server/constant/country.go
@@ -1,29 +1,29 @@
 package constant
 
-var Countires = map[string]Country{
-	"CA": {
-		Code:  "CA",
-		Code3: "CAN",
-		Num:   "124",
-		Name:  "Canada",
-	},
-	"PK": {
-		Code:  "PK",
-		Code3: "PAK",
-		Num:   "586",
-		Name:  "Pakistan",
-	},
-	"US": {
-		Code:  "US",
-		Code3: "USA",
-		Num:   "840",
-		Name:  "United States of America",
-	},
+type Country struct {
+	IsoCode  string
+	IsoCode3 string
+	Num      string
+	Name     string
 }
 
-type Country struct {
-	Code  string
-	Code3 string
-	Num   string
-	Name  string
+var Countires = map[string]Country{
+	"CA": {
+		IsoCode:  "CA",
+		IsoCode3: "CAN",
+		Num:      "124",
+		Name:     "Canada",
+	},
+	"PK": {
+		IsoCode:  "PK",
+		IsoCode3: "PAK",
+		Num:      "586",
+		Name:     "Pakistan",
+	},
+	"US": {
+		IsoCode:  "US",
+		IsoCode3: "USA",
+		Num:      "840",
+		Name:     "United States of America",
+	},
 }
