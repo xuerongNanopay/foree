3c185dff91cd380cf72bf9f03fc5638d597ad2c1
xuerongNanopay <xuerong@nanopay.net>
2024-09-05 14:44:39 -0400

    add scheme for password

--- foree-mobile/constants/validation_sheme.js
+++ foree-mobile/constants/validation_sheme.js
@@ -81,12 +81,22 @@ const PositiveFloatScheme = ({
 }
 
 // Minimum eight characters, at least one letter and one number:
-// "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+const PasswdMinFourOneLetterOneNumber = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/
+// Minimum eight characters, at least one letter and one number:
+const PasswdMinEightOneLetterOneNumber = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+// Minimum eight characters, at least one letter, one number and one special character:
+const PasswdMinEightOneLetterOneNumberOneSpecial = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+// Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+const PasswdMinEightOneUpperOneLowerOneNumber = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
+const PasswdMinEightOneUpperOneLowerOneNumberOneSepcia = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
 
 const PasswordScheme =({
-  required=true
+  required=true,
+  passwdLevel=PasswdMinFourOneLetterOneNumber
 }) => {
-  const ret = Joi.string().regex(/^$/)
+
+  const ret = Joi.string().regex(passwdLevel)
   return required ? ret.required() : ret
 }
 
@@ -99,5 +109,11 @@ export default {
   IntegerScheme,
   PositiveIntegerScheme,
   FloatScheme,
-  PositiveFloatScheme
+  PositiveFloatScheme,
+  PasswordScheme,
+  PasswdMinFourOneLetterOneNumber,
+  PasswdMinEightOneLetterOneNumber,
+  PasswdMinEightOneLetterOneNumberOneSpecial,
+  PasswdMinEightOneUpperOneLowerOneNumber,
+  PasswdMinEightOneUpperOneLowerOneNumberOneSepcia
 };
