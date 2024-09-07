b9429313538b986f28abe951baee2be0580d139b
xuerongNanopay <xuerong@nanopay.net>
2024-09-06 20:45:45 -0400

    add SliderDropDown component

---
+++ foree-mobile/components/SliderDropDown.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SliderDropDown = () => {
+  return (
+    <View>
+      <Text>SliderDropDown</Text>
+    </View>
+  )
+}
+
+export default SliderDropDown
