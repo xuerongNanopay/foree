c5d5b59f10eb3dbf454583cea3a8c40feb25c5e5
xuerongNanopay <xuerong@nanopay.net>
2024-09-08 19:29:27 -0400

    refactor regoin

--- foree-server/constant/region.go
+++ foree-server/constant/region.go
@@ -1,135 +1,135 @@
 package constant
 
 type Region struct {
-	Code     string
-	IsoCode3 string
-	Country  string
-	Name     string
+	Code    string
+	IsoCode string
+	Country string
+	Name    string
 }
 
 var Regions = map[string]map[string]Region{
 	"PK": {
-		"JK": {
-			Code:     "JK",
-			IsoCode3: "PK-JK",
-			Name:     "Azad Jammu and Kashmir",
-			Country:  "PK",
+		"PK-JK": {
+			Code:    "JK",
+			IsoCode: "PK-JK",
+			Name:    "Azad Jammu and Kashmir",
+			Country: "PK",
 		},
-		"BA": {
-			Code:     "BA",
-			IsoCode3: "PK-BA",
-			Name:     "Balochistan",
-			Country:  "PK",
+		"PK-BA": {
+			Code:    "BA",
+			IsoCode: "PK-BA",
+			Name:    "Balochistan",
+			Country: "PK",
 		},
-		"GB": {
-			Code:     "GB",
-			IsoCode3: "PK-GB",
-			Name:     "Gilgit-Baltistan",
-			Country:  "PK",
+		"PK-GB": {
+			Code:    "GB",
+			IsoCode: "PK-GB",
+			Name:    "Gilgit-Baltistan",
+			Country: "PK",
 		},
-		"IS": {
-			Code:     "IS",
-			IsoCode3: "PK-IS",
-			Name:     "Islamabad",
-			Country:  "PK",
+		"PK-IS": {
+			Code:    "IS",
+			IsoCode: "PK-IS",
+			Name:    "Islamabad",
+			Country: "PK",
 		},
-		"KP": {
-			Code:     "KP",
-			IsoCode3: "PK-KP",
-			Name:     "Khyber Pakhtunkhwa",
-			Country:  "PK",
+		"PK-KP": {
+			Code:    "KP",
+			IsoCode: "PK-KP",
+			Name:    "Khyber Pakhtunkhwa",
+			Country: "PK",
 		},
-		"PB": {
-			Code:     "PB",
-			IsoCode3: "PK-PB",
-			Name:     "Punjab",
-			Country:  "PK",
+		"PK-PB": {
+			Code:    "PB",
+			IsoCode: "PK-PB",
+			Name:    "Punjab",
+			Country: "PK",
 		},
-		"SD": {
-			Code:     "SD",
-			IsoCode3: "PK-SD",
-			Name:     "Sindh",
-			Country:  "PK",
+		"PK-SD": {
+			Code:    "SD",
+			IsoCode: "PK-SD",
+			Name:    "Sindh",
+			Country: "PK",
 		},
 	},
 	"CA": {
-		"AB": {
-			Code:     "AB",
-			IsoCode3: "CA-AB",
-			Name:     "Alberta",
-			Country:  "CA",
+		"CA-AB": {
+			Code:    "AB",
+			IsoCode: "CA-AB",
+			Name:    "Alberta",
+			Country: "CA",
 		},
-		"BC": {
-			Code:     "BC",
-			IsoCode3: "CA-BC",
-			Name:     "British Columbia",
-			Country:  "CA",
+		"CA-BC": {
+			Code:    "BC",
+			IsoCode: "CA-BC",
+			Name:    "British Columbia",
+			Country: "CA",
 		},
-		"MB": {
-			Code:     "MB",
-			IsoCode3: "CA-MB",
-			Name:     "Manitoba",
-			Country:  "CA",
+		"CA-MB": {
+			Code:    "MB",
+			IsoCode: "CA-MB",
+			Name:    "Manitoba",
+			Country: "CA",
 		},
-		"NB": {
-			Code:     "NB",
-			IsoCode3: "CA-NB",
-			Name:     "New Brunswick",
-			Country:  "CA",
+		"CA-NB": {
+			Code:    "NB",
+			IsoCode: "CA-NB",
+			Name:    "New Brunswick",
+			Country: "CA",
 		},
-		"NL": {
-			Code:     "NL",
-			IsoCode3: "CA-NL",
-			Name:     "Newfoundland and Labrador",
-			Country:  "CA",
+		"CA-NL": {
+			Code:    "NL",
+			IsoCode: "CA-NL",
+			Name:    "Newfoundland and Labrador",
+			Country: "CA",
 		},
-		"NT": {
-			Code:     "NT",
-			IsoCode3: "CA-NT",
-			Name:     "Northwest Territories",
-			Country:  "CA",
+		"CA-NT": {
+			Code:    "NT",
+			IsoCode: "CA-NT",
+			Name:    "Northwest Territories",
+			Country: "CA",
 		},
-		"NS": {
-			Code:     "NS",
-			IsoCode3: "CA-NS",
-			Name:     "Nova Scotia",
-			Country:  "CA",
+		"CA-NS": {
+			Code:    "NS",
+			IsoCode: "CA-NS",
+			Name:    "Nova Scotia",
+			Country: "CA",
 		},
-		"NU": {
-			Code:     "NU",
-			IsoCode3: "CA-NU",
-			Name:     "Nunavut",
-			Country:  "CA",
+		"CA-NU": {
+			Code:    "NU",
+			IsoCode: "CA-NU",
+			Name:    "Nunavut",
+			Country: "CA",
 		},
-		"ON": {
-			Code:     "ON",
-			IsoCode3: "CA-ON",
-			Name:     "Ontario",
-			Country:  "CA",
+		"CA-ON": {
+			Code:    "ON",
+			IsoCode: "CA-ON",
+			Name:    "Ontario",
+			Country: "CA",
 		},
-		"PE": {
-			Code:     "PE",
-			IsoCode3: "CA-PE",
-			Name:     "Prince Edward Island",
-			Country:  "CA",
+		"CA-PE": {
+			Code:    "PE",
+			IsoCode: "CA-PE",
+			Name:    "Prince Edward Island",
+			Country: "CA",
 		},
-		"QC": {
-			Code:     "QC",
-			IsoCode3: "CA-QC",
-			Name:     "Quebec",
-			Country:  "CA",
+		"CA-QC": {
+			Code:    "QC",
+			IsoCode: "CA-QC",
+			Name:    "Quebec",
+			Country: "CA",
 		},
-		"SK": {
-			Code:     "SK",
-			IsoCode3: "CA-SK",
-			Name:     "Saskatchewan",
-			Country:  "CA",
+		"CA-SK": {
+			Code:    "SK",
+			IsoCode: "CA-SK",
+			Name:    "Saskatchewan",
+			Country: "CA",
 		},
-		"YT": {
-			Code:     "YT",
-			IsoCode3: "CA-YT",
-			Name:     "Yukon",
-			Country:  "CA",
+		"CA-YT": {
+			Code:    "YT",
+			IsoCode: "CA-YT",
+			Name:    "Yukon",
+			Country: "CA",
 		},
 	},
 }
