8b5be56cf4add191ee95c3625167aa56256a0c96
xuerongNanopay <xuerong@nanopay.net>
2024-09-08 15:58:23 -0400

    ass string util

---
+++ foree-mobile/util/string_util.js
@@ -0,0 +1,29 @@
+containSubsequence = (s, q, {caseInsensitive=false}) => {
+    if ( typeof s !== 'string' && typeof s !== typeof q) return false
+    if ( q.length > s.length ) return false
+    if ( caseInsensitive ) {
+        s.toLowerCase()
+        q.toLowerCase()
+    }
+
+    let p1 = 0;
+    let p2 = 0;
+    
+    while(p1 < q.length) {
+      if (q[p1] === s[p2]) {
+        p1++;
+        p2++;
+      } else {
+        p2++;
+        if (p2 > s.length) {
+          return false;
+        }
+      }
+    }
+  
+    return true;
+}
+
+export default {
+    containSubsequence
+}
