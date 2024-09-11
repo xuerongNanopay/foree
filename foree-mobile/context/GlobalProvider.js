0a31dfb64bfba869f21256748a92bd61e11610aa
xuerongNanopay <xuerong@nanopay.net>
2024-09-11 14:29:26 -0400

    plugin global provider

--- foree-mobile/context/GlobalProvider.js
+++ foree-mobile/context/GlobalProvider.js
@@ -6,7 +6,7 @@ const GlobalContext = createContext()
 
 export const useGlobalContext = () => useContext(GlobalContext)
 
-const GlobalProvider = ({ children }) => {
+export const GlobalProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [user, setUser] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
