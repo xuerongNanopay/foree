86a884cfcfbd81e68ecfab27fb09e37ebf67de3c
xuerongNanopay <xuerong@nanopay.net>
2024-09-14 15:32:41 -0400

    format project structure

--- foree-mobile/app/_layout.jsx
+++ foree-mobile/app/_layout.jsx
@@ -33,9 +33,9 @@ const RootLayout = () => {
     <GlobalProvider>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false}} />
-        <Stack.Screen name="(contact)" options={{ headerShown: false}} />
         <Stack.Screen name="(auth)" options={{ headerShown: false}} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+        <Stack.Screen name="(app)" options={{ headerShown: false}} />
+        <Stack.Screen name="(contact)" options={{ headerShown: false}} />
         {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false}} /> */}
       </Stack>
     </GlobalProvider>
