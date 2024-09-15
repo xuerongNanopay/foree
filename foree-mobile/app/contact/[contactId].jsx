538593d0cda89078f6149711cb882d61ff803f5f
xuerongNanopay <xuerong@nanopay.net>
2024-09-15 16:27:59 -0400

    save cahnge

--- foree-mobile/app/contact/[contactId].jsx
+++ foree-mobile/app/contact/[contactId].jsx
@@ -1,9 +1,12 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, ScrollView } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
 import React, { useEffect, useState } from 'react'
 
 import { accountService } from '../../service'
 import { formatContactName } from '../../util/contact_util'
+import Regions from '../../constants/region'
+import Countries from '../../constants/country'
+import { ContactTransferMethods } from '../../constants/contacts'
 
 const ContactDetail = () => {
   const {contactId} = useLocalSearchParams()
@@ -31,7 +34,7 @@ const ContactDetail = () => {
   }, [])
   return (
     <SafeAreaView>
-      <View className="px-2 py-4">
+      <View className="flex h-full px-2 py-4">
         <View className="flex flex-row items-center pb-4 border-b-[1px] border-slate-300">
           <Text className="flex-1 font-pbold text-lg">{!!contact ? formatContactName(contact, 20) : ""}</Text>
           <TouchableOpacity
@@ -43,6 +46,68 @@ const ContactDetail = () => {
             <Text className="font-pextrabold text-white">Send</Text>
           </TouchableOpacity>
         </View>
+        {/* Contact Detail */}
+        <ScrollView className="flex-1 mt-4 mb-2">
+          <View className="bg-slate-200 border-[1px] border-slate-500 rounded-lg py-6 px-2">
+            {/* Name */}
+            <View className="border-b-[1px] border-slate-400 pb-2">
+              <Text className="font-semibold mb-2">
+                First Name: <Text className="text-slate-600">{contact?.firstName}</Text>
+              </Text>
+              { !!contact?.middleName ? <>
+                  <Text className="font-semibold mb-2">
+                    Middle Name: <Text className="text-slate-600">{contact?.middleName}</Text>
+                  </Text>
+                </> : null
+              }
+              <Text className="font-semibold">
+                Last Name: <Text className="text-slate-600">{contact?.lastName}</Text>
+              </Text>
+            </View>
+            {/* Address */}
+            <View className="border-b-[1px] border-slate-400 pt-2">
+              <Text className="font-semibold mb-2">
+                Address Line 1: <Text className="text-slate-600">{contact?.address1}</Text>
+              </Text>
+              { !!contact?.address2 ? <>
+                  <Text className="font-semibold mb-2">
+                    Address Line 2: <Text className="text-slate-600">{contact?.address2}</Text>
+                  </Text>
+                </> : null
+              }
+              <Text className="font-semibold mb-2">
+                City: <Text className="text-slate-600">{contact?.city}</Text>
+              </Text>
+              <Text className="font-semibold mb-2">
+                Province: <Text className="text-slate-600">{!!contact?.province ? Regions["PK"]?.[contact.province].name : ""}</Text>
+              </Text>
+              <Text className="font-semibold mb-2">
+                Country: <Text className="text-slate-600">{!!contact?.country ? Countries[contact.country]?.name : ""}</Text>
+              </Text>
+              { !!contact?.postalCode ? <>
+                  <Text className="font-semibold mb-2">
+                    Postal Code: <Text className="text-slate-600">{contact?.postalCode}</Text>
+                  </Text>
+                </> : null
+              }
+              { !!contact?.phoneNumber ? <>
+                  <Text className="font-semibold mb-2">
+                    Phone Number: <Text className="text-slate-600">{contact?.phoneNumber}</Text>
+                  </Text>
+                </> : null
+              }
+            </View>
+            {/* Destination */}
+            <View className="mt-2">
+              <Text className="font-semibold mb-2">
+                Contact Relationship: <Text className="text-slate-600">{contact?.relationshipToContact}</Text>
+              </Text>
+              <Text className="font-semibold mb-2">
+                Transfer Method: <Text className="text-slate-600">{!!contact?.transferMethod ? (ContactTransferMethods.find(x => x.value === contact.transferMethod)?.name || contact.transferMethod): ""}</Text>
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaView>
   )
