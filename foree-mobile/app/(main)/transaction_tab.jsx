4287aba8dfcefe8c0d836cd122f50c3b3b6aebf2
xuerongNanopay <xuerong@nanopay.net>
2024-09-15 19:02:59 -0400

    add transaction tab

--- foree-mobile/app/(main)/transaction_tab.jsx
+++ foree-mobile/app/(main)/transaction_tab.jsx
@@ -1,11 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const TransactionTab = () => {
   return (
-    <View>
-      <Text>TransactionTab</Text>
-    </View>
+    <SafeAreaView>
+      <View className="flex h-full px-4 pt-4">
+        <View className="pb-4 mb-4 border-b-[1px] border-slate-300">
+          <View className="flex flex-row items-center">
+            <Text className="flex-1 font-pbold text-2xl">Transactions</Text>
+            <TouchableOpacity
+              onPress={()=> {router.push("/contact/create")}}
+              activeOpacity={0.7}
+              className="bg-[#1A6B54] py-2 px-4 rounded-full"
+              disabled={false}
+            >
+              <Text className="font-pextrabold text-white">Send</Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
