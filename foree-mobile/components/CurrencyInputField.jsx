eef292f1659828dba33f2b29455489441aa29b94
xuerongNanopay <xuerong@nanopay.net>
2024-09-16 11:53:41 -0400

    add currencyInputField

---
+++ foree-mobile/components/CurrencyInputField.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CurrencyInputField = () => {
+  return (
+    <View>
+      <Text>CurrencyInputField</Text>
+    </View>
+  )
+}
+
+export default CurrencyInputField
