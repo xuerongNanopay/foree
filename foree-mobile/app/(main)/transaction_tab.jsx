c4232dad98228e971ba27cebad31ed5c69c53168
xuerongNanopay <xuerong@nanopay.net>
2024-09-15 20:53:17 -0400

    add static picker

--- foree-mobile/app/(main)/transaction_tab.jsx
+++ foree-mobile/app/(main)/transaction_tab.jsx
@@ -1,7 +1,51 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList } from 'react-native'
+import React, { useState } from 'react'
+
+const transactionStatuses = [
+  {
+    id:"All"
+  },
+  { 
+    id:"Initial"
+  },
+  { 
+    id:"Await Payment"
+  },
+  { 
+    id:"In Progress"
+  },
+  { 
+    id:"Completed"
+  },
+  { 
+    id:"Cancelled"
+  },
+  { 
+    id:"Ready To Pickup"
+  },
+  { 
+    id:"Refunding"
+  },
+  { 
+    id:"Refunded"
+  }
+]
 
 const TransactionTab = () => {
+  const [selectedStatus, setSelectedStatus] = useState('All')
+
+  const statusChipItem = ({item}) => {
+    const bgColor = selectedStatus === item.id ? "bg-green-200" : ""
+    return (
+      <TouchableOpacity 
+        onPress={() => setSelectedStatus(item.id)}
+        className={`p-2 border-[1px] border-green-800 rounded-2xl mr-2 ${bgColor}`}
+      >
+        <Text className="text-green-800">{item.id}</Text>
+      </TouchableOpacity>
+    )
+  }
+
   return (
     <SafeAreaView>
       <View className="flex h-full px-4 pt-4">
@@ -18,13 +62,24 @@ const TransactionTab = () => {
               <Text className="font-pextrabold text-white">Send</Text>
             </TouchableOpacity>
           </View>
+          {/* Status */}
+          <FlatList
+            data={transactionStatuses}
+            renderItem={statusChipItem}
+            keyExtractor={item => item.id}
+            showsHorizontalScrollIndicator={false}
+            horizontal={true}
+            className="mb-1"
+          />
           {/* Status Pagenation */}
           <View className="flex flex-row items-center">
             <View className="flex-1">
-              <Text>111</Text>
+              <View className>
+                <Text className="text-green-800 font-psemibold">{selectedStatus}</Text>
+              </View>
             </View>
             <View className="flex flex-row items-center">
-              <Text className="mr-2">1-50 of 22,248</Text>
+              <Text className="mr-2">1-50</Text>
               <TouchableOpacity
                 onPress={()=> {console.log("TODO: transaction left")}}
                 activeOpacity={0.7}
@@ -41,10 +96,6 @@ const TransactionTab = () => {
                 <Text className="text-2xl">▶️</Text>
               </TouchableOpacity>
             </View>
-          </View>
-          {/* Status */}
-          <View>
-            
           </View>
         </View>
       </View>
