65950cd3eac2bba604d92b9fadc064d6f269d74e
xuerongNanopay <xuerong@nanopay.net>
2024-09-15 20:02:45 -0400

    add pagenation

--- foree-mobile/app/(main)/transaction_tab.jsx
+++ foree-mobile/app/(main)/transaction_tab.jsx
@@ -5,8 +5,9 @@ const TransactionTab = () => {
   return (
     <SafeAreaView>
       <View className="flex h-full px-4 pt-4">
-        <View className="pb-4 mb-4 border-b-[1px] border-slate-300">
-          <View className="flex flex-row items-center">
+        <View className="pb-2 mb-4 border-b-[1px] border-slate-300">
+          {/* Title */}
+          <View className="flex flex-row items-center pb-2 mb-2 border-b-[1px] border-slate-300">
             <Text className="flex-1 font-pbold text-2xl">Transactions</Text>
             <TouchableOpacity
               onPress={()=> {router.push("/contact/create")}}
@@ -17,8 +18,33 @@ const TransactionTab = () => {
               <Text className="font-pextrabold text-white">Send</Text>
             </TouchableOpacity>
           </View>
+          {/* Status Pagenation */}
+          <View className="flex flex-row items-center">
+            <View className="flex-1">
+              <Text>111</Text>
+            </View>
+            <View className="flex flex-row items-center">
+              <Text className="mr-2">1-50 of 22,248</Text>
+              <TouchableOpacity
+                onPress={()=> {console.log("TODO: transaction left")}}
+                activeOpacity={0.7}
+                disabled={false}
+                className="mr-2"
+              >
+                <Text className="text-2xl">◀️</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={()=> {console.log("TODO: transaction right")}}
+                activeOpacity={0.7}
+                disabled={false}
+              >
+                <Text className="text-2xl">▶️</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          {/* Status */}
           <View>
-
+            
           </View>
         </View>
       </View>
