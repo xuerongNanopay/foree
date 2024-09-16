b908a3e509f482b6124e6210a505881c0cc4043e
xuerongNanopay <xuerong@nanopay.net>
2024-09-16 13:30:13 -0400

    format

--- foree-mobile/components/CurrencyInputField.jsx
+++ foree-mobile/components/CurrencyInputField.jsx
@@ -1,5 +1,5 @@
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, TextInput, Modal } from 'react-native'
+import React, { useState } from 'react'
 
 const variants = {
   bordered: "border-2 rounded-2xl",
@@ -13,30 +13,44 @@ const CurrencyInputField = ({
   inputContainerStyles,
   supportCurrency=[],
   value,
+  onCurrencyChange=()=>{}
 }) => {
+  const [visible, setVisible] = useState(false)
+  const [currency, setCurrency] = useState("")
+  const [amount, setAmount] = useState(0.0)
+
   return (
-    <View className={`${containerStyles}`}>
-      <Text className="test-base test-gray-100 font-pmedium mb-2">{title}</Text>
-      <View
-        className={`
-          flex flex-row items-center
-          h-12 ${variants[variant] ?? variants.bordered}
-          border-slate-400 focus:border-secondary-200 ${inputContainerStyles}
-        `}
-      >
-        <TouchableOpacity
-          onPress={()=>{console.log("press currency")}}
-          activeOpacity={0.7}
-          className="bg-slate-200 h-full px-2 rounded-l-2xl flex justify-center"
+    <>
+      <View className={`${containerStyles}`}>
+        <Text className="test-base test-gray-100 font-pmedium mb-2">{title}</Text>
+        <View
+          className={`
+            flex flex-row items-center
+            h-12 ${variants[variant] ?? variants.bordered}
+            border-slate-400 focus:border-secondary-200 ${inputContainerStyles}
+          `}
         >
-          <Text className="font-semibold">🇨🇦 CAD 🔽</Text>
-        </TouchableOpacity>
-        <TextInput
-          keyboardType='decimal-pad'
-          className="flex-1 h-full text-right px-2 font-semibold"
-        />
+          <TouchableOpacity
+            onPress={()=>{console.log("press currency")}}
+            activeOpacity={0.7}
+            className="bg-slate-200 h-full px-2 rounded-l-2xl flex justify-center"
+          >
+            <Text className="font-semibold">🇨🇦 CAD 🔽</Text>
+          </TouchableOpacity>
+          <TextInput
+            keyboardType='decimal-pad'
+            className="flex-1 h-full text-right px-2 font-semibold"
+          />
+        </View>
       </View>
-    </View>
+      {/* Currency picker */}
+      <Modal
+        visible={visible}
+        animationType='slide'
+      >
+      
+      </Modal>
+    </>
   )
 }
 
