1f109b3cdbb65203dc59278ab120871ecbead077
xuerongNanopay <xuerong@nanopay.net>
2024-09-29 01:10:06 -0400

    update style

--- foree-mobile/components/TxSummaryChip.jsx
+++ foree-mobile/components/TxSummaryChip.jsx
@@ -11,39 +11,39 @@ const TxSummaryStatusRefunding    = "Refunding"
 
 const SummaryTxStatusChipStyles = {
   [TxSummaryStatusInitial]: {
-    borderColor: "border-slate-600",
-    textColor: "text-slate-600",
-    bgColor: "bg-slate-300",
+    borderColor: "border-slate-800",
+    textColor: "text-slate-800",
+    bgColor: "bg-slate-200",
   },
   [TxSummaryStatusAwaitPayment]: {
-    borderColor: "border-yellow-600",
-    textColor: "text-yellow-600",
-    bgColor: "bg-yellow-100",
+    borderColor: "border-yellow-800",
+    textColor: "text-yellow-800",
+    bgColor: "bg-yellow-200",
   },
   [TxSummaryStatusInProgress]: {
-    borderColor: "border-purple-600",
-    textColor: "text-purple-600",
-    bgColor: "bg-purple-100",
+    borderColor: "border-purple-800",
+    textColor: "text-purple-800",
+    bgColor: "bg-purple-200",
   },
   [TxSummaryStatusCompleted]: {
-    borderColor: "border-green-600",
-    textColor: "text-green-600",
-    bgColor: "bg-green-100",
+    borderColor: "border-green-800",
+    textColor: "text-green-800",
+    bgColor: "bg-green-200",
   },
   [TxSummaryStatusCancelled]: {
-    borderColor: "border-red-600",
-    textColor: "text-red-600",
-    bgColor: "bg-red-100",
+    borderColor: "border-red-800",
+    textColor: "text-red-800",
+    bgColor: "bg-red-200",
   },
   [TxSummaryStatusPickup]: {
-    borderColor: "border-yellow-600",
-    textColor: "text-yellow-600",
-    bgColor: "bg-yellow-100",
+    borderColor: "border-yellow-800",
+    textColor: "text-yellow-800",
+    bgColor: "bg-yellow-200",
   },
   [TxSummaryStatusRefunding]: {
-    borderColor: "border-yellow-600",
-    textColor: "text-yellow-600",
-    bgColor: "bg-yellow-100",
+    borderColor: "border-yellow-800",
+    textColor: "text-yellow-800",
+    bgColor: "bg-yellow-200",
   },
 }
 
