844d8db67f5cf32ca2ce8aae1cef4853229c1249
xuerongNanopay <xuerong@nanopay.net>
2024-09-29 19:03:35 -0400

    bug fix

--- foree-server/app/foree/constant/tx.go
+++ foree-server/app/foree/constant/tx.go
@@ -6,6 +6,7 @@ var AllowTransactionsStatus = map[string]bool{
 	string(transaction.TxSummaryStatusInitial):      true,
 	string(transaction.TxSummaryStatusAwaitPayment): true,
 	string(transaction.TxSummaryStatusInProgress):   true,
+	string(transaction.TxSummaryStatusPickup):       true,
 	string(transaction.TxSummaryStatusCompleted):    true,
 	string(transaction.TxSummaryStatusCancelled):    true,
 	string(transaction.TxSummaryStatusRefunding):    true,
