367b3c625381655b23c3cb46435ca18c74ec089c
xuerongNanopay <xuerong@nanopay.net>
2024-10-02 15:32:54 -0400

    format file

--- foree-mobile/app/settings/_layout.jsx
+++ foree-mobile/app/settings/_layout.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const ProfileLayout = () => {
+const SettingLayout = () => {
   return (
     <View>
-      <Text>ProfileLayout</Text>
+      <Text>SettingLayout</Text>
     </View>
   )
 }
 
-export default ProfileLayout
+export default SettingLayout
