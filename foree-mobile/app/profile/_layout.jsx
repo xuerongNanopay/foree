f46e181a9b45566d83cefedfa87f584e31bea7ea
xuerongNanopay <xuerong@nanopay.net>
2024-10-01 20:56:40 -0400

    setup initial files for profile

---
+++ foree-mobile/app/profile/_layout.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfileLayout = () => {
+  return (
+    <View>
+      <Text>ProfileLayout</Text>
+    </View>
+  )
+}
+
+export default ProfileLayout
--- foree-mobile/app/profile/update_passwd.jsx
+++ foree-mobile/app/profile/update_passwd.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Search = () => {
+const UpdatePasswd = () => {
   return (
     <View>
-      <Text>Search</Text>
+      <Text>UpdatePasswd</Text>
     </View>
   )
 }
 
-export default Search
+export default UpdatePasswd
