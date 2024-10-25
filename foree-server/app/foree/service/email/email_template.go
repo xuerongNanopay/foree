4b95ad620a8a73910f15c4abff36545465193c38
xuerongNanopay <xuerong@nanopay.net>
2024-10-24 21:17:22 -0400

    add email template

--- foree-server/app/foree/service/email/email_template.go
+++ foree-server/app/foree/service/email/email_template.go
@@ -1,3 +1,106 @@
 package foree_email_service
 
-const ()
+const ForeeRootTemplate = `
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
+	<title>{{appName}}</title>
+	<style type="text/css" media="screen">
+	@media screen {
+		* { font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif !important; }
+	}
+	</style>
+	<style type="text/css">
+	* {
+		font-family: /*%FONT1%*/, Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	}
+	body {
+		-webkit-font-smoothing: antialiased;
+		-webkit-text-size-adjust: none;
+		width: 100%;
+		margin: 0;
+		height: 100%;
+	}
+	a {
+		text-decoration: none !important;
+	}
+	p {
+		margin: 0;
+	}
+	.p-body {
+		font-size: 1rem;
+		line-height: 1.5;
+		font-style: normal;
+		margin:  0;
+	}
+	.subheading {
+		font-size: 1.25rem;
+		line-height: 1.6;
+		font-style: normal;
+		font-weight: 700;
+		margin:  0;
+	}
+	.legal {
+		font-size: 0.75rem;
+		color: #8D8D8D;
+		text-align: center;
+	}
+	.link {
+		color: #406DEA;
+	}
+	.wrapper {
+		padding: 0.4rem;
+	}
+	@media only screen and (min-width: 768px) {
+		.wrapper {
+		padding: 1.5rem;
+		}
+	}
+	</style>
+</head>
+<body style = "background: #EAECED;font-family: 'Source Sans Pro', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; padding:1rem;">
+		<div class="wrapper" style="background: white; border-radius: 0.25rem; margin-bottom: 1.25rem;" >
+		<div class="appLogo" style="margin-bottom: 1.5rem;">
+			<a href="{{appLink}}" title="{{appName}} Logo" target="_blank" style="cursor:default"><img src= "{{logo}}"  alt="{{appName}} Logo" style="max-width: 115px;" /></a>
+		</div>
+
+		<div class="content">
+			<content>TODO</content>
+		</div>
+		</div>
+
+		<div style="text-align:center">
+		<div style="display:inline-block;">
+			<a href="https://facebook.com/foree.remit" title="Facebook logo" target="_blank" style="cursor:default">
+			<img src="{{appLink}}/images/media/facebook-logo.png"  alt="Foree Facebook" style="max-width: 115px;" />
+			</a>
+
+			<a href="https://www.instagram.com/foree_remit/" title="Instagram logo" target="_blank" style="cursor:default">
+			<img src="{{appLink}}/images/media/instagram-logo.png"  alt="Foree Instagram" style="max-width: 115px;" />
+			</a>
+
+			<a href="https://www.linkedin.com/company/foree-remit/" title="LinkedIn logo" target="_blank" style="cursor:default">
+			<img src="{{appLink}}/images/media/linkedin-logo.png"  alt="Foree LinkedIn" style="max-width: 115px;" />
+			</a>
+		</div>
+
+		<div class="legal">
+			<div style="margin: 4px;">To manage notification settings, please <a href="{{appLink}}/#notification-settings">click here</a>.</div>
+			<div style="margin: 4px;">This message was sent to {{sendTo}}</div>
+			<div style="margin: 4px;">© Foree Remittance, {{supportAddress}}</div>
+		</div>
+
+		<div style="text-align:center;">
+			<a href="{{privacyUrl}}" >{{privacyLabel}}</a> |
+			<a href="{{termsAndCondLink}}" >{{termsAndCondLabel}}</a>
+			<br>
+			<a href="mailto:{{supportEmail}}" > Contact Us </a> |
+			<a href="https://foree.nanopay.net/" > About Us </a>
+		</div>
+
+		</div>
+</body>
+</html>
+`
--- foree-server/app/foree/service/email/email_test.go
+++ foree-server/app/foree/service/email/email_test.go
@@ -1,1 +1,24 @@
 package foree_email_service
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"testing"
+)
+
+func TestHowHTMLTemplateWorks(t *testing.T) {
+	t.Run("Demo 1", func(t *testing.T) {
+		buf := new(bytes.Buffer)
+		template, _ := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+		_ = template.ExecuteTemplate(buf, "T", "<script>alert('you have been pwned')</script>")
+		fmt.Println(buf.String())
+	})
+
+	t.Run("Test Root Layout", func(t *testing.T) {
+		buf := new(bytes.Buffer)
+		template, _ := template.New("foo").Parse(`{{define "T"}}Hello, {{.}}!{{end}}`)
+		_ = template.ExecuteTemplate(buf, "T", "<script>alert('you have been pwned')</script>")
+		fmt.Println(buf.String())
+	})
+}
