969609db5c8597d68af13544b3a73837d3c289ab
xuerongNanopay <xuerong@nanopay.net>
2024-10-31 14:26:21 -0400

    add contact email template

--- foree-server/app/foree/service/email/template/contact_template.go
+++ foree-server/app/foree/service/email/template/contact_template.go
@@ -12,3 +12,15 @@ const ContactAddTemplate = `
 	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">If you think you have received this email in error, please contact us at: <a href="mailto:{{.SupportEmail}}" style="text-decoration: none;">{{.SupportEmail}}</a></p>
 </main>
 `
+const ContactRemoveTemplate = `
+<main>
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">Dear {{.CustomerName}}, </p>
+
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">This is a confirmation that {{.ContactName}} has been removed from Contacts on your Foree Remittance account.</p>
+
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">Thanks, </p>
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">Team Foree Remittance</p>
+
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">If you think you have received this email in error, please contact us at: <a href="mailto:{{.SupportEmail}}" style="text-decoration: none;">{{.SupportEmail}}</a></p>
+</main>
+`
