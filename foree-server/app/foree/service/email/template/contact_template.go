516331a676c61b483ee6ca8a48c11ad8cc365780
xuerongNanopay <xuerong@nanopay.net>
2024-10-31 13:55:44 -0400

    package refactor

---
+++ foree-server/app/foree/service/email/template/contact_template.go
@@ -0,0 +1,14 @@
+package foree_email_template
+
+const ContactAddTemplate = `
+<main>
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">Dear {{.CustomerName}}, </p>
+
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">This is a confirmation that {{.ContactName}} has been added as a Contact to your Foree Remittance account.</p>
+
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">Thanks, </p>
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">Team Foree Remittance</p>
+
+	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">If you think you have received this email in error, please contact us at: <a href="mailto:{{.SupportEmail}}" style="text-decoration: none;">{{.SupportEmail}}</a></p>
+</main>
+`
--- foree-server/app/foree/service/email/template/transaction_template.go
+++ foree-server/app/foree/service/email/template/transaction_template.go
@@ -3,7 +3,7 @@ package foree_email_template
 const TransactionCancelledTemplate = `
 <main>
 	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">
-		Dear {{.GreetingName}},
+		Dear {{.CustomerName}},
 	</p> 
 	<br>
 	<p style="font-family: /*%FONT1%*/ Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif;  font-size: 16px;  line-height: 1.5; margin: 0;">
