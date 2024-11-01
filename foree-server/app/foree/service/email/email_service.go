9c58b925d32167bb5b31804925935966d1f37b22
xuerongNanopay <xuerong@nanopay.net>
2024-10-31 22:27:43 -0400

    complete email service

--- foree-server/app/foree/service/email/email_service.go
+++ foree-server/app/foree/service/email/email_service.go
@@ -2,8 +2,21 @@ package foree_email_service
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 )
+
+//Check this one for attachment: https://gist.github.com/douglasmakey/90753ecf37ac10c25873825097f46300
+
+type EmailMsg struct {
+	From        string
+	To          string
+	CC          []string
+	BCC         []string
+	Subject     string
+	Body        string
+	Attachments map[string][]byte
+}
 
 type emailTemplate struct {
 	name       string
@@ -24,6 +37,7 @@ type BasicTemplateCfg struct {
 	TermsAndCondLabel string
 	ContactEmail      string
 	AboutLink         string
+	SupportEmail      string
 }
 
 type ServiceConfig struct {
@@ -33,16 +47,26 @@ type ServiceConfig struct {
 	Password string
 }
 
+const (
+	emailVerifyCodeTPLName      = "EMAIL_VERIFY_CODE"
+	contactAddedKeyTPLName      = "CONTACT_ADDED"
+	contactRemovedKeyTPLName    = "CONTACT_REMOVED"
+	transactionInitiatedTPLName = "TRANSACTION_INITIATED"
+	transactionPickupTPLName    = "TRANSACTION_PICKUP"
+	transactionCompletedTPLName = "TRANSACTION_COMPLETED"
+	transactionCancelledTPLName = "TRANSACTION_CANCELLED"
+)
+
 func NewEmailService() *EmailService {
 	ret := &EmailService{}
 	ret.templates = map[string]emailTemplate{
-		"EMAIL_VERIFY_CODE":     buildTemplate("EMAIL_VERIFY_CODE", "Verify your email to activate your Foree Remittance account", emailVerifyCodeHTML, rootLayoutTemplateHTML),
-		"CONTACT_ADDED":         buildTemplate("CONTACT_ADDED", "Foree Remittance - Contact added", ContactAddHTML, rootLayoutTemplateHTML),
-		"CONTACT_REMOVED":       buildTemplate("CONTACT_REMOVED", "Foree Remittance - Contact removed", contactRemoveHTML, rootLayoutTemplateHTML),
-		"TRANSACTION_INITIATED": buildTemplate("TRANSACTION_INITIATED", "Foree Remittance - New Transaction Initiated", transactionInitiatedHTML, rootLayoutTemplateHTML),
-		"TRANSACTION_PICKUP":    buildTemplate("TRANSACTION_PICKUP", "Foree Remittance - Your cash transaction is available for pick-up", transactionPickupHTML, rootLayoutTemplateHTML),
-		"TRANSACTION_COMPLETED": buildTemplate("TRANSACTION_COMPLETED", "Foree Remittance - Transaction completed", transactionCompletedHTML, rootLayoutTemplateHTML),
-		"TRANSACTION_CANCELLED": buildTemplate("TRANSACTION_CANCELLED", "Foree Remittance - Transaction cancelled", transactionCancelledHTML, rootLayoutTemplateHTML),
+		emailVerifyCodeTPLName:      buildTemplate(emailVerifyCodeTPLName, "Verify your email to activate your Foree Remittance account", emailVerifyCodeHTML, rootLayoutTemplateHTML),
+		contactAddedKeyTPLName:      buildTemplate(contactAddedKeyTPLName, "Foree Remittance - Contact added", ContactAddHTML, rootLayoutTemplateHTML),
+		contactRemovedKeyTPLName:    buildTemplate(contactRemovedKeyTPLName, "Foree Remittance - Contact removed", contactRemoveHTML, rootLayoutTemplateHTML),
+		transactionInitiatedTPLName: buildTemplate(transactionInitiatedTPLName, "Foree Remittance - New Transaction Initiated", transactionInitiatedHTML, rootLayoutTemplateHTML),
+		transactionPickupTPLName:    buildTemplate(transactionPickupTPLName, "Foree Remittance - Your cash transaction is available for pick-up", transactionPickupHTML, rootLayoutTemplateHTML),
+		transactionCompletedTPLName: buildTemplate(transactionCompletedTPLName, "Foree Remittance - Transaction completed", transactionCompletedHTML, rootLayoutTemplateHTML),
+		transactionCancelledTPLName: buildTemplate(transactionCancelledTPLName, "Foree Remittance - Transaction cancelled", transactionCancelledHTML, rootLayoutTemplateHTML),
 	}
 
 	return ret
@@ -54,27 +78,69 @@ type EmailService struct {
 	templates        map[string]emailTemplate
 }
 
-func (e *EmailService) sendEmail(name string, data templateData) error {
-	tpl, _ := e.templates[name]
+func (e *EmailService) buildEmailMsg(tplName string, data templateData, from, to string) (*EmailMsg, error) {
+	data.AppName = e.basicTemplateCfg.AppName
+	data.AppLink = e.basicTemplateCfg.AppLink
+	data.LogoImg = e.basicTemplateCfg.LogoImg
+	data.SendTo = e.basicTemplateCfg.SendTo
+	data.SupportAddress = e.basicTemplateCfg.SupportAddress
+	data.PrivacyUrl = e.basicTemplateCfg.PrivacyUrl
+	data.PrivacyLabel = e.basicTemplateCfg.PrivacyLabel
+	data.TermsAndCondLink = e.basicTemplateCfg.TermsAndCondLink
+	data.TermsAndCondLabel = e.basicTemplateCfg.TermsAndCondLabel
+	data.ContactEmail = e.basicTemplateCfg.ContactEmail
+	data.AboutLink = e.basicTemplateCfg.AboutLink
+
+	tpl, ok := e.templates[tplName]
+	if !ok {
+		return nil, fmt.Errorf("email template `%v` not found", tplName)
+	}
 
 	buf := new(bytes.Buffer)
 	err := tpl.contentTpl.Execute(buf, data)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	data.Content = template.HTML(buf.String())
 	buf.Reset()
 	err = tpl.layoutTpl.Execute(buf, data)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	emailBody := buf.String()
-	if emailBody == "" {
-		//TODO: send email use goEmail
-	}
 
+	return &EmailMsg{
+		From:    from,
+		To:      to,
+		Subject: tpl.subject,
+		Body:    emailBody,
+	}, nil
+}
+
+func (e *EmailService) sendWithTemplate(tplName string, data templateData, from, to string) error {
+	eMsg, err := e.buildEmailMsg(tplName, data, from, to)
+	if err != nil {
+		return err
+	}
+	if err := e.send(eMsg.Subject, eMsg.Body, eMsg.From, eMsg.To); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *EmailService) send(subject, body, from, to string) error {
+	return nil
+}
+
+func (e *EmailService) SendEmailVerifyCode(emailVerifyCode, from, to string) error {
+	data := templateData{
+		EmailVerifyCode: emailVerifyCode,
+	}
+	if err := e.sendWithTemplate(emailVerifyCodeTPLName, data, from, to); err != nil {
+		return err
+	}
 	return nil
 }
 
