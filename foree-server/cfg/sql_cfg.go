0db41ae76a5f180af518cad89517d968bbdf1064
xuerongNanopay <xuerong@nanopay.net>
2024-11-02 21:05:08 -0400

    add generic method

--- foree-server/cfg/sql_cfg.go
+++ foree-server/cfg/sql_cfg.go
@@ -19,6 +19,37 @@ type configWrapper struct {
 	expiredAt time.Time
 }
 
+func (c *SQLCFG) loadCfg(name string, converter func(conf *configuration) any) (any, error) {
+	if cfg, ok := c.configs.Load(name); ok {
+		w := cfg.(configWrapper)
+		return w.config, nil
+	}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if cfg, ok := c.configs.Load(name); ok {
+		w := cfg.(configWrapper)
+		return w.config, nil
+	}
+
+	conf, err := c.repo.getUniqueConfigurationByName(context.TODO(), name)
+	if err != nil {
+		return name, err
+	}
+	if conf == nil {
+		return name, fmt.Errorf("configuraion `%v` not found", name)
+	}
+
+	cw := configWrapper{
+		rawValue:  conf.RawValue,
+		expiredAt: time.Now().Add(time.Millisecond * time.Duration(conf.RefreshInterval)),
+		config:    converter(conf),
+	}
+
+	c.configs.Store(name, cw)
+	return cw.config, nil
+}
+
 func (c *SQLCFG) LoadStringCfg(name string) (StringConfig, error) {
 	if cfg, ok := c.configs.Load(name); ok {
 		w := cfg.(configWrapper)
