537f7b93cadc1a82ed4f0f89b59b6c66789675cf
xuerongNanopay <xuerong@nanopay.net>
2024-11-02 21:28:43 -0400

    complete config

--- foree-server/cfg/sql_cfg.go
+++ foree-server/cfg/sql_cfg.go
@@ -3,6 +3,7 @@ package cfg
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -51,35 +52,77 @@ func (c *SQLCFG) loadCfg(name string, converter func(conf *configuration) any) (
 }
 
 func (c *SQLCFG) LoadStringCfg(name string) (StringConfig, error) {
-	if cfg, ok := c.configs.Load(name); ok {
-		w := cfg.(configWrapper)
-		return w.config.(StringConfig), nil
-	}
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	cfg, err := c.loadCfg(name, func(conf *configuration) any {
+		v := new(string)
+		*v = conf.RawValue
+		return StringConfig{
+			v: v,
+		}
+	})
 
-	if cfg, ok := c.configs.Load(name); ok {
-		w := cfg.(configWrapper)
-		return w.config.(StringConfig), nil
-	}
-
-	conf, err := c.repo.getUniqueConfigurationByName(context.TODO(), name)
 	if err != nil {
 		return StringConfig{}, err
 	}
-	if conf == nil {
-		return StringConfig{}, fmt.Errorf("configuraion `%v` not found", name)
+
+	return cfg.(StringConfig), nil
+}
+
+func (c *SQLCFG) LoadIntCfg(name string) (IntConfig, error) {
+	cfg, err := c.loadCfg(name, func(conf *configuration) any {
+		v := new(int)
+		i, err := strconv.Atoi(conf.RawValue)
+		if err != nil {
+			return err
+		}
+		*v = i
+		return IntConfig{
+			v: v,
+		}
+	})
+
+	if err != nil {
+		return IntConfig{}, err
 	}
 
-	v := conf.RawValue
-	cw := configWrapper{
-		rawValue:  conf.RawValue,
-		expiredAt: time.Now().Add(time.Millisecond * time.Duration(conf.RefreshInterval)),
-		config: StringConfig{
-			v: &v,
-		},
+	return cfg.(IntConfig), nil
+}
+
+func (c *SQLCFG) LoadInt64Cfg(name string) (Int64Config, error) {
+	cfg, err := c.loadCfg(name, func(conf *configuration) any {
+		v := new(int64)
+		i, err := strconv.ParseInt(conf.RawValue, 10, 64)
+		if err != nil {
+			return err
+		}
+		*v = i
+		return Int64Config{
+			v: v,
+		}
+	})
+
+	if err != nil {
+		return Int64Config{}, err
 	}
 
-	c.configs.Store(name, cw)
-	return cw.config.(StringConfig), nil
+	return cfg.(Int64Config), nil
+}
+
+func (c *SQLCFG) LoadBoolCfg(name string) (BoolConfig, error) {
+	cfg, err := c.loadCfg(name, func(conf *configuration) any {
+		v := new(bool)
+		i, err := strconv.ParseBool(conf.RawValue)
+		if err != nil {
+			return err
+		}
+		*v = i
+		return BoolConfig{
+			v: v,
+		}
+	})
+
+	if err != nil {
+		return BoolConfig{}, err
+	}
+
+	return cfg.(BoolConfig), nil
 }
