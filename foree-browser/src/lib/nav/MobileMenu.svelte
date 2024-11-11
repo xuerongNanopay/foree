ad578cc6c5c441e4be2ab323bd0b7d78e7c6d214
xuerongNanopay <xuerong@nanopay.net>
2024-11-11 11:23:49 -0500

    components layout

---
+++ foree-browser/src/lib/nav/MobileMenu.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+</script>
+
+<div id="mobile-menu-item" class="mobile">
+    <div class="modal-overlay" aria-hidden="true"></div>
+    <div class="menu">
+        <div class="mobile-main-menu">
+            <div class="menu-background ready" style:height="100%"></div>
+            <div class="clip">
+                <div>fdsa</div>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+                <h1>AAA</h1>
+            </div>
+        </div>
+    </div>
+</div>
