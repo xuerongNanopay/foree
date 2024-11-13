8d3adf8e0f46e2381d27d2294d33b51d3db20027
xuerongNanopay <xuerong@nanopay.net>
2024-11-12 23:46:27 -0500

    add accordion for meny

--- foree-browser/src/lib/nav/MobileSubMenu.svelte
+++ foree-browser/src/lib/nav/MobileSubMenu.svelte
@@ -1,3 +1,61 @@
 <script lang="ts">
-    
+
 </script>
+
+<ul class="accordion">
+    <li>
+        <input type="radio" name="accordion" id="first" checked>
+        <label for="first">Menu</label>
+        <ul class="content">
+            <li>Dashboard</li>
+            <li>Send</li>
+            <li>Send</li>
+            <li>Send</li>
+            <li>Send</li>
+        </ul>
+    </li>
+    <br/>
+    <li>
+        <input type="radio" name="accordion" id="second">
+        <label for="second">Profile</label>
+        <ul class="content">
+            <li>me</li>
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+            <li>About</li>
+        </ul>
+    </li>
+</ul>
+
+<style>
+    .accordion {
+        background: salmon;
+    }
+
+    .accordion input[type="radio"] {
+        display: none;
+    }
+
+    .accordion .content {
+        max-height: 0;
+        overflow: hidden;
+        transition: max-height 1s ease-in-out;
+    }
+
+    .accordion input[type="radio"]:checked + label + .content {
+        max-height: 600px;
+    }
+
+    .accordion input[type="radio"] + label::before {
+        content: "+";
+        margin-right: 10px;
+        font-size: 24px;
+        font-weight: 600;
+    }
+
+    .accordion input[type="radio"]:checked + label::before {
+        content: "-";
+    }
+</style>
