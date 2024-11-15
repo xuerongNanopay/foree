fbb407693c76a79562f1a41a8a95dd3eac175b55
xuerongNanopay <xuerong@nanopay.net>
2024-11-14 23:45:45 -0500

    Initial the transfer form

--- foree-browser/src/routes/(app)/(mutation)/create_transaction/+page.svelte
+++ foree-browser/src/routes/(app)/(mutation)/create_transaction/+page.svelte
@@ -1,4 +1,55 @@
 <script lang="ts">
+    import MultiStepForm from "$lib/components/MultiStepForm.svelte"
+
 
 </script>
 
+{#snippet step1()}
+    <div class="step">
+        <h2>Transaction Details</h2>
+        <p>Enter the details for your transaction.</p>
+        <div class="remittance-fields">
+        </div>
+    </div>
+{/snippet}
+
+<main>
+    <MultiStepForm
+        steps={[step1]}
+        method="POST"
+    >
+
+    </MultiStepForm>
+</main>
+
+<style>
+    main {
+        width: 95%;
+        max-width: 900px;
+        margin: 3.5rem auto;
+        border: 1px solid saddlebrown;
+
+        @media (max-width: 700px) {
+            margin: 2.5rem auto;
+        }
+    }
+
+    .step {
+        overflow: hidden;
+        & > p:first-of-type {
+            text-align: center;
+            margin: 1.5rem 0;
+        }
+
+        & > h2:first-of-type {
+            text-align: center;
+            margin: 0;
+        }
+
+        @media (max-width: 400px) {
+            & > h2:first-of-type {
+                font-size: 1em;
+            }
+        }
+    }
+</style>
