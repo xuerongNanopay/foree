cb4d0b401d4728706b914b7fde452daeb707e53f
xuerongNanopay <xuerong@nanopay.net>
2024-11-28 18:31:55 -0500

    add anchor

--- foree-ui/packages/utilities/icons/src/routes/+page.svelte
+++ foree-ui/packages/utilities/icons/src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import Anchor from "$lib/Anchor.svelte"
+</script>
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<Anchor/>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
