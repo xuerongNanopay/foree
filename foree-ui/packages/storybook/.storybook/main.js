f5b675d81fe0ed8c1b589d8c22c0f6e03a0bd860
xuerongNanopay <xuerong@nanopay.net>
2024-11-29 00:58:32 -0500

    refactor story

--- foree-ui/packages/storybook/.storybook/main.js
+++ foree-ui/packages/storybook/.storybook/main.js
@@ -1,5 +1,8 @@
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
+  stories: [
+    "../../utilities/icons/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   framework: '@storybook/svelte-vite',
   addons: [
     "@storybook/addon-svelte-csf",
