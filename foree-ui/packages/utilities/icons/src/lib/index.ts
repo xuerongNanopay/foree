d501b52bc7478d8e249d733db7271de8bb33cd8f
xuerongNanopay <xuerong@nanopay.net>
2024-12-01 00:27:01 -0500

    add check and avatar icon

--- foree-ui/packages/utilities/icons/src/lib/index.ts
+++ foree-ui/packages/utilities/icons/src/lib/index.ts
@@ -2,3 +2,5 @@
 export { default as AnchorIcon } from './Anchor.svelte';
 export { default as ArrowLeftIcon } from './ArrowLeft.svelte';
 export { default as ArrowRightIcon } from './ArrowRight.svelte';
+export { default as AvatarIcon } from './Avatar.svelte';
+export { default as CheckIcon } from './Check.svelte';
