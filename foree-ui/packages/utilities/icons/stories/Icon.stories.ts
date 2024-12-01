020d682cb3401d4987926dd31610fbefbf0b6965
xuerongNanopay <xuerong@nanopay.net>
2024-11-30 21:57:24 -0500

    add story book icon

--- foree-ui/packages/utilities/icons/stories/Icon.stories.ts
+++ foree-ui/packages/utilities/icons/stories/Icon.stories.ts
@@ -2,15 +2,15 @@ import type { Meta, StoryObj } from '@storybook/svelte';
 import Icon from './Icon.svelte';
 
 const meta: Meta<typeof Icon> = {
-    title: 'icons/Button',
-    component: Icon,
+  title: 'icons/Button',
+  component: Icon,
 };
 
 type Story = StoryObj<typeof meta>;
 
-export default meta;
-
 export const Primary: Story = {
-    args: {
-    },
-  };
+  args: {
+  },
+};
+
+export default meta;
--- foree-ui/packages/utilities/icons/stories/Icon.svelte
+++ foree-ui/packages/utilities/icons/stories/Icon.svelte
@@ -1,1 +1,24 @@
-<h1>11111</h1>
+<script lang="ts">
+  import './icon.css';
+  import * as allIcons from '../src/lib';
+  import type { Component } from 'svelte';
+  import type { SVGAttributes } from 'svelte/elements';
+
+  const icons: {name: string, component: Component<SVGAttributes<EventTarget>>}[] = []
+  
+  for (let n of Object.keys(allIcons)) {
+    icons.push({name: n, component: allIcons[n]})
+  }
+  const { size= 'medium' } = $props();
+
+</script>
+
+{#each icons as icon}
+  <div class="cell">
+    <div class="cell__icon__container">
+      <icon.component/>
+    </div>
+    <!-- <small class="cell__desp">{icon.name.replace('Icon', '').toLowerCase()}</small> -->
+    <small class="cell__desp">{'1111111111111'}</small>
+  </div>
+{/each}
