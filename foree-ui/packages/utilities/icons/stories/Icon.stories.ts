0a8c47e6f1d2f5b683cbb16131f7891152d20e49
xuerongNanopay <xuerong@nanopay.net>
2024-11-30 22:56:47 -0500

    update storybook menu name for icon

--- foree-ui/packages/utilities/icons/stories/Icon.stories.ts
+++ foree-ui/packages/utilities/icons/stories/Icon.stories.ts
@@ -2,14 +2,18 @@ import type { Meta, StoryObj } from '@storybook/svelte';
 import Icon from './Icon.svelte';
 
 const meta: Meta<typeof Icon> = {
-  title: 'icons/Button',
+  title: 'icons',
   component: Icon,
 };
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Icon>;
 
-export const Primary: Story = {
-  args: {
+export const SupportIcons: Story = {
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large', 'xlarge'],
+    }
   },
 };
 
--- foree-ui/packages/utilities/icons/stories/Icon.svelte
+++ foree-ui/packages/utilities/icons/stories/Icon.svelte
@@ -5,23 +5,28 @@
   import type { SVGAttributes } from 'svelte/elements';
 
   const icons: {name: string, component: Component<SVGAttributes<EventTarget>>}[] = []
+
+  type Prop = {
+    size: 'small' | 'medium' | 'large' | 'xlarge',
+  }
   
   for (let n of Object.keys(allIcons)) {
     for (let _ of Array(140)) {
       icons.push({name: n, component: allIcons[n]})
     }
   }
-  const { size= 'medium' } = $props();
+  const { size='medium' }: Prop = $props();
 
   function formatCellName(name: string): string {
     return (name.slice(0,1).toLocaleLowerCase() + name.slice(1)).replace('Icon', '').replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
   }
 </script>
 
+<!-- TODO: why typescript error -->
 {#snippet cell(icon)}
   <div class="cell">
     <div class="cell__icon__container">
-      <icon.component/>
+      <icon.component class={`cell__icon--${size}`}/>
     </div>
     <small class="cell__desp">{formatCellName(icon.name)}</small>
   </div>
@@ -29,12 +34,6 @@
 
 <div>
   {#each icons as icon}
-    <!-- <div class="cell">
-      <div class="cell__icon__container">
-        <icon.component/>
-      </div>
-      <small class="cell__desp">{formatCellName(icon.name)}</small>
-    </div> -->
     {@render cell(icon)}
   {/each}
 </div>
